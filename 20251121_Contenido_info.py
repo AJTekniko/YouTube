5543c7991eea6b1c4d8a9bc9ca57c8ea2f8cc3cd
AJTekniko <ajt3chmail@gmail.com>
2025-11-21 23:41:24 -0700

    Create 20251121_Contenido_info.py

---
+++ 20251121_Contenido_info.py
@@ -0,0 +1,7 @@
+"""Cómo producir un calendario en Python:
+
+Se puede producir un calendario en Python por medio de la función month. Considera el siguiente ejemplo, importamos el módulo calender e imprimimos la función con el año y el mes deseado como sus argumentos."""
+
+import calendar
+
+print(calendar.month(2025, 11))
