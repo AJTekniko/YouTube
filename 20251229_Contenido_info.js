d67fa782fb3a8ca88d0bed95e9e4c7e292f6d63b
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2025-12-29 23:44:00 -0700

    Create 20251229_Contenido_info.js

---
+++ 20251229_Contenido_info.js
@@ -0,0 +1,7 @@
+/*Cómo se convierte un número binario en un número decimal usando JavaScript:
+
+Para empezar, podemos almacenar el número binario en una variable. Usamos la función parseInt() para convertirlo en un número decimal. Después simplemente lo mandamos a la consola.*/
+
+let binario = 101
+let decimal = parseInt(binario, 2);
+console.log(decimal);
