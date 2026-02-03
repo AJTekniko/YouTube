c2f91ceb4f3a7e33ba018d2fb408c6a2adc06dc3
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-02-02 23:57:09 -0700

    Added video link

--- 20251113_Contenido_info.py
+++ 20251113_Contenido_info.py
@@ -1,4 +1,6 @@
 """
+Video: youtube.com/watch?v=oSMqxhJNMig
+
 Cómo rotar los contenidos de una lista en Python:
 
 Para empezar, podemos crear una función personalizada que acepta dos parámetros: la lista y la cantidad de movimiento. Usamos el operador módulo para tratar con rotaciones más grandes que la lista. Retornamos el final y el principio de la lista concatenados. Después simplemente imprimimos.
