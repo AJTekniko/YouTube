8f12034eaec3e55c076916b0d2ada061a7d6d704
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-02-06 23:24:17 -0700

    Update comments and add video link in script

--- 20251101_Contenido_info.py
+++ 20251101_Contenido_info.py
@@ -1,5 +1,10 @@
-"""Cómo determinar cuales números de una lista son divisibles por otro número en Python:
-Se puede determinar por medio de la función filter. Considera el siguiente ejemplo, obtenemos el número y lo almacenamos en una variable. Lo comparamos con los números en la lista en una función temporaria o lambda y poblamos una nueva variable con la función filter. Después simplemente imprimimos."""
+"""
+Video: youtube.com/shorts/t50ayeTQK6w
+
+Cómo determinar cuales números de una lista son divisibles por otro número en Python:
+
+Se puede determinar por medio de la función filter. Considera el siguiente ejemplo, obtenemos el número y lo almacenamos en una variable. Lo comparamos con los números en la lista en una función temporaria o lambda y poblamos una nueva variable con la función filter. Después simplemente imprimimos.
+"""
 
 la_lista = [12, 65 , 54, 39, 102, 339, 221]
 numero = int(input("Por favor escriba un número: "))
