936ff2e3430bd88bf64c5852cb8edcfa4e50ac6d
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-02-10 23:57:23 -0700

    Create 20260210_Contenido_info.js
    Instructions on creating a JS file.

---
+++ 20260210_Contenido/20260210_Contenido_info.js
@@ -0,0 +1,7 @@
+/*
+Video: youtube.com/shorts/C1rlsWtpdIA
+
+Cómo crear un archivo de JavaScript:
+
+Se puede crear un archivo de JavaScript con simplemente guardar el archivo usando la extensión correcta. Por ejemplo, en un editor de texto como Bloc de notas (también conocido como Notepad), podemos guardar el archivo con la extensión "js" para que sea interpretado como un archivo de JavaScript.
+*/
