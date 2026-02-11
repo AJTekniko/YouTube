5a7071348f0e8c58c4aa489576b339e5674c6126
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-02-10 23:49:41 -0700

    Create 20260210_Contenido_info.py
    Information on creating a Python file.

---
+++ 20260210_Contenido/20260210_Contenido_info.py
@@ -0,0 +1,7 @@
+"""
+Video: youtube.com/shorts/QKeAy341j-A
+
+Cómo crear un archivo de Python:
+
+Se puede crear un archivo de Python con simplemente guardar el archivo usando la extensión correcta. Por ejemplo, en un editor de texto como Bloc de notas (también conocido como Notepad), podemos guardar el archivo con la extensión "py" para que sea interpretado como un archivo de Python.
+"""
