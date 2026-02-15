be65e08976c6b5bb1b22f5a697f7e7760378bf60
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-02-14 23:49:39 -0700

    Add video link

--- 20251105_Contenido_info.py
+++ 20251105_Contenido_info.py
@@ -1,6 +1,10 @@
-"""Cómo crear una matriz en Python:
+"""
+Video: youtube.com/watch?v=IySyW49o17Y
 
-Para empezar, podemos crear una función personalizada que acepta dos parámetros: el número de filas y el número de columnas. Creamos un ciclo for con el número de columnas como rango en la forma de una lista adentro de otra lista que contiene un ciclo for con el número de filas como rango y lo asignamos a una variable. Retornamos la matriz. Después simplemente imprimimos."""
+Cómo crear una matriz en Python:
+
+Para empezar, podemos crear una función personalizada que acepta dos parámetros: el número de filas y el número de columnas. Creamos un ciclo for con el número de columnas como rango en la forma de una lista adentro de otra lista que contiene un ciclo for con el número de filas como rango y lo asignamos a una variable. Retornamos la matriz. Después simplemente imprimimos.
+"""
 
 def crea_matriz(filas, columnas):
     matriz = [[0 for columna in range(columnas)] for fila in range(filas)] 
