e587d35cd19de1d28ab961422cd27b207767028e
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-02-17 23:57:34 -0700

    Add video link

--- 20251106_Contenido_info.py
+++ 20251106_Contenido_info.py
@@ -1,6 +1,10 @@
-"""Cómo determinar el día de la semana con una fecha en Python:
+"""
+Video: youtube.com/watch?v=i_eadJfxGzk
 
-Para empezar, podemos importar la clase datetime del módulo datetime. Por medio de una función personalizada, se puede rebanar la cadena de caracteres con la fecha y almacenar el año, el mes y el día en diferentes variables en forma de números enteros. Con esos valores, se puede determinar el día de la semana utilizando la función datetime y su método strftime, y almacenar el resultado en una variable. Después solo queda retornar la variable con el día de la semana e imprimir."""
+Cómo determinar el día de la semana con una fecha en Python:
+
+Para empezar, podemos importar la clase datetime del módulo datetime. Por medio de una función personalizada, se puede rebanar la cadena de caracteres con la fecha y almacenar el año, el mes y el día en diferentes variables en forma de números enteros. Con esos valores, se puede determinar el día de la semana utilizando la función datetime y su método strftime, y almacenar el resultado en una variable. Después solo queda retornar la variable con el día de la semana e imprimir.
+"""
 
 from datetime import datetime
 
