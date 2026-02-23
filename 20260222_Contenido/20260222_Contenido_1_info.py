aaf662dc342620ba5b471f0766494b8f1af2cebe
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-02-22 23:46:40 -0700

    Create 20260222_Contenido_1_info.py

---
+++ 20260222_Contenido/20260222_Contenido_1_info.py
@@ -0,0 +1,3 @@
+"""
+Esta semana, yo subí videos hablando de los atributos required, minlength y maxlength en HTML. También publiqué una pregunta de entrevista con la respuesta en Python sobre la ampliación de matrices. En JavaScript, expliqué cómo convertir varios valores a la misma vez, cómo determinar si un número es número primo y cómo comparar números proporcionados para encontrar el número más grande. ¿Cuál fue su video favorito? Por favor escríbalo en los comentarios. Gracias.
+"""
