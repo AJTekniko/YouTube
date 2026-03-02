9c5e7477a0050ef16a5a0b09ba68f31cdea0a1e8
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-03-01 23:41:02 -0700

    Create 20260301_Contenido_1_info.py

---
+++ 20260301_Contenido/20260301_Contenido_1_info.py
@@ -0,0 +1,26 @@
+"""
+Video: youtube.com/shorts/Bptca7vArWg
+
+Esta semana, yo subí videos hablando de cómo encontrar el factorial de un número con recursión y sin recursión por medio de JavaScript. También publiqué una pregunta de entrevista con la respuesta en Python sobre la submatriz contigua que contiene la suma más grande. Adicionalmente, expliqué cómo crear una tabla de multiplicar y cómo producir una sucesión de Fibonacci con recursión y sin recursión por medio de JavaScript. Los enlaces de los videos se pueden encontrar en la descripción de este video. ¿Cuál fue su video favorito? Por favor escríbalo en los comentarios. Muchas gracias.
+
+Cómo se encuentra el factorial de un número con JavaScript:
+youtube.com/shorts/tg956z5ewHw
+
+Cómo se encuentra el factorial de un número con JavaScript usando recursión:
+youtube.com/shorts/A76JyjivVf8
+
+Pregunta de entrevista (20260223):
+youtube.com/shorts/hc0mPExb1Ds
+
+Pregunta de Entrevista (20260223) [Respuesta con Python]:
+youtube.com/shorts/p6SGqRa8_OA
+
+Cómo producir una tabla de multiplicar con JavaScript:
+youtube.com/shorts/BSq0QTfZ_Eo
+
+Cómo producir una sucesión de Fibonacci con el ciclo for en JavaScript:
+youtube.com/shorts/6vp9uh5ltSg
+
+Cómo producir una sucesión de Fibonacci con recursión en JavaScript:
+youtube.com/shorts/T76VhJwSEEs
+"""
