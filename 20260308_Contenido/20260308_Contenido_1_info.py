43b84f84094dac1500c403312888937245d80c30
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-03-08 23:42:20 -0600

    Create 20260308_Contenido_1_info.py

---
+++ 20260308_Contenido/20260308_Contenido_1_info.py
@@ -0,0 +1,5 @@
+"""
+Video: youtube.com/shorts/br4qOZzTKWU
+
+Esta semana, yo subí videos de JavaScript explicando cómo determinar si un número es número narcisista, cómo convertir una cantidad de tiempo en horas, minutos y segundos, cómo sustituir valores en una cadena de caracteres, cómo agregar dos matrices, cómo determinar los números divisibles por otro número en una matriz, y cómo producir el máximo común divisor de dos números. También publiqué una pregunta de entrevista con la respuesta en Python sobre el elemento mayoritario de una matriz. ¿Cuál fue su video favorito? Por favor escríbalo en los comentarios. Muchas gracias.
+"""
