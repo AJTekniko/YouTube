445e96337ff5d227c37560f0ac85db74eaf63e7a
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-03-12 23:53:11 -0600

    Add video link

--- 20251108_Contenido_info.py
+++ 20251108_Contenido_info.py
@@ -1,4 +1,6 @@
 """
+Video: youtube.com/watch?v=SvPaa5iAV4c
+
 Cómo evaluar la longitud de cadenas de caracteres en Python:
 
 Para evaluar la longitud de cadenas de caracteres, se puede utlizar la función len. Considera el siguiente ejemplo, creamos una función personalizada que acepta una cadena de caracteres y por medio de sentencias condicionales, evaluamos la condición que le corresponde y retornamos el mensaje apropiado. Después simplemente imprimimos.
