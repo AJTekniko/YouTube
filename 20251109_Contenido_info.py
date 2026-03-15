a092da3fa29d423c295de8eb51fcfab66a5664e6
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-03-14 23:04:19 -0600

    Add video link

--- 20251109_Contenido_info.py
+++ 20251109_Contenido_info.py
@@ -1,4 +1,6 @@
 """
+Video: youtube.com/watch?v=J1W9C29LgCE
+
 Cómo producir el mínimo común múltiplo con Python:
 
 Para empezar, podemos crear una función personalizada que acepta dos parámetros proporcionados que son almacenados en forma de números enteros. Son comparados por medio de sentencias condicionales y el más grande es almacenado en una variable. Luego por medio de un ciclo while, una sentencia condicional y el operador módulo determinamos si el número con mayor valor es divisible por ambos números y si no, incrementamos el número por uno hasta llegar al número que sí es divisible por ambos números, y retornamos el valor. Después simplemente imprimimos.
