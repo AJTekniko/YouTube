1382111e03acf9e6bbe392b5fcd2a385cd824560
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-03-15 23:06:47 -0600

    Create 20260315_Contenido_1_info.py

---
+++ 20260315_Contenido/20260315_Contenido_1_info.py
@@ -0,0 +1,5 @@
+"""
+Video: youtube.com/shorts/oOoba52aXwc
+
+Esta semana, yo subí videos de JavaScript explicando cómo producir el máximo común divisor de dos números con el algoritmo euclidiano y cómo retornar una matriz que contiene un término en específico. También publiqué un video que explica cómo se crean olas con CSS y una pregunta de entrevista con la respuesta en Python, que trata con retornar la cantidad mínima de cambios necesarios para transformar una cadena de caracteres en una cadena alterna. ¿Cuál fue su video favorito? Por favor escríbalo en los comentarios. Muchas gracias.
+"""
