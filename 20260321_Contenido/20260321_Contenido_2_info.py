b31cd07c2159e9658010ae1d0468bdc2dc8ce385
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-03-21 18:59:56 -0600

    Create 20260321_Contenido_2_info.py

---
+++ 20260321_Contenido/20260321_Contenido_2_info.py
@@ -0,0 +1,7 @@
+"""
+Video: youtube.com/shorts/I7qczeaXExU
+
+Nueva composición:
+
+Produje un instrumental para el concurso del usuario Brainless. Pero como no uso itch.io o discord, no lo entregué. De todos modos les quise compartir una parte. Por favor dejen su opinión en los comentarios. Muchas gracias.
+"""
