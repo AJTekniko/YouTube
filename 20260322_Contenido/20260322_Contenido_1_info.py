e544d8b8c78e671229e90d7c15261fa39dfc57ef
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-03-22 23:53:44 -0600

    Create 20260322_Contenido_1_info.py

---
+++ 20260322_Contenido/20260322_Contenido_1_info.py
@@ -0,0 +1,5 @@
+"""
+Video: youtube.com/shorts/Fyny77xlDUc
+
+Esta semana, yo subí videos de Python y JavaScript explicando cómo eliminar espacios, cómo retornar varios valores, cómo se invierte un número, y cómo calcular la cantidad de dígitos en un número. También subí videos de CSS que hablan sobre la estructura básica de una regla, y las propiedades width, height, margin, y padding. Hasta subí un instrumental que produje para un concurso. ¿Cuál fue su video favorito? Por favor escríbalo en los comentarios. Muchas gracias.
+"""
