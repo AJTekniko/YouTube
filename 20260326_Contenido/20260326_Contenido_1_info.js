f7bceeacd803813c3f859483617fa9f856d8e2bc
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-03-27 18:30:04 -0600

    Create 20260326_Contenido_1_info.js

---
+++ 20260326_Contenido/20260326_Contenido_1_info.js
@@ -0,0 +1,14 @@
+/*
+Video: youtube.com/shorts/G_GXMja7i3E
+
+Cómo crear comentarios con JavaScript:
+
+Se pueden crear comentarios de una línea usando dos diagonales (//). Para crear comentarios multilíneas, se usa una diagonal y un asterisco para iniciar el comentario, y un asterisco seguido por una diagonal para finalizarlo. Considere el ejemplo que ve en pantalla.
+*/
+
+// Este es un comentario de una línea
+
+/*
+Este es un
+comentario multilínea
+*/
