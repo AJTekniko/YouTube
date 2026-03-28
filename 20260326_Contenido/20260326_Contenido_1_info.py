bbcd720d144af3c355c4710abb95fc6dca3e32bd
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-03-27 18:27:39 -0600

    Create 20260326_Contenido_1_info.py

---
+++ 20260326_Contenido/20260326_Contenido_1_info.py
@@ -0,0 +1,14 @@
+"""
+Video: youtube.com/shorts/F0uu4cT3_Us
+
+Cómo crear comentarios con Python:
+
+Se pueden crear comentarios de una línea usando el signo de número (#). Para crear comentarios multilíneas, se usan tres comillas para iniciar el comentario y tres comillas para finalizarlo. Considere el ejemplo que ve en pantalla.
+"""
+
+# Este es un comentario de una línea
+
+"""
+Este es un
+comentario multilínea
+"""
