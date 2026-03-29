0a958ee42d737fb5e65a1d2f82949754b9acd9f7
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-03-28 23:53:08 -0600

    Add video link

--- 20251112_Contenido_info.js
+++ 20251112_Contenido_info.js
@@ -1,4 +1,6 @@
 /*
+Video: www.youtube.com/watch?v=1qPnNZdnJQo
+
 Cómo remover acentos en JavaScript:
 
 Para empezar, creamos una función personalizada que acepta una cadena de caracteres que contiene acentos como parámetro. Usamos el método normalize con "NFD" como argumento seguido por el método replace con los argumentos que ve en pantalla para filtrar los caracteres que combinan marcas diacríticas, y retornamos el resultado. Después simplemente lo mandamos a la consola.
