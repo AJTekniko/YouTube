ceeb595e01f5136909935936b37ee9644f73e1ae
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-03-29 23:41:16 -0600

    Create 20260329_Contenido_1_info.py

---
+++ 20260329_Contenido/20260329_Contenido_1_info.py
@@ -0,0 +1,5 @@
+"""
+Video: youtube.com/shorts/NKUFy2o7M14
+
+Esta semana, yo subí videos de Python y JavaScript explicando cómo determinar si dos cadenas son anagramas, cómo remover elementos duplicados de una lista o matriz, y cómo rebanar cadenas. Subí videos de CSS que hablan sobre cómo ajustar el espacio en una lista, las propiedades list-style, y los diferentes estados de un enlace. También publiqué videos de Python, JavaScript, HTML, y CSS explicando cómo crear comentarios. ¿Cuál fue su video favorito? Por favor escríbalo en los comentarios. Muchas gracias.
+"""
