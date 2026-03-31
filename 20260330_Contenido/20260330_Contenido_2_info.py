724832f6c48e73f6e5dbd241506e460ccb1516b5
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-03-30 23:59:39 -0600

    Create 20260330_Contenido_2_info.py

---
+++ 20260330_Contenido/20260330_Contenido_2_info.py
@@ -0,0 +1,13 @@
+"""
+Video: youtube.com/shorts/R5mI8vYTK8k
+
+Cómo determinar el tipo de dato de una variable con Python:
+
+Se puede usar la función type() para obtener el tipo de dato de una variable. Considere el ejemplo que ve en pantalla.
+
+saludo = "Hola a todos 😎"
+edad = 21
+
+print(type(saludo))
+print(type(edad))
+"""
