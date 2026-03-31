67e69c3c665a594f6d13393a862c662aea968019
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-03-30 23:41:50 -0600

    Create 20260330_Contenido_1_info.py

---
+++ 20260330_Contenido/20260330_Contenido_1_info.py
@@ -0,0 +1,11 @@
+"""
+Video: youtube.com/shorts/Sr2FStfwI_Y
+
+La convención de nombres para variables en Python:
+
+•Las variables solo pueden tener nombres que comienzan con una letra o un guion bajo.
+•Las variables solo pueden tener caracteres alfanuméricos y guiones bajos.
+•Se distingue entre mayúsculas y minúsculas. Por ejemplo, si existen nombres de variables con los mismos caracteres, pero son diferentes en cuanto a mayúsculas y minúsculas, los nombres son considerados únicos (edad, Edad, y EDAD serían diferentes variables).
+•Las variables no pueden tener palabras claves como sus nombres.
+•Los nombres se escriben en snake case, o el estilo de escritura en el que los espacios son reemplazados con guiones bajos.
+"""
