66eaef6acb83cb761117e6de5acbab7342245b30
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-03-31 00:18:07 -0600

    Create 20260330_Contenido_2_info.js

---
+++ 20260330_Contenido/20260330_Contenido_2_info.js
@@ -0,0 +1,13 @@
+/*
+Video: youtube.com/shorts/kbnw-WyPdJo
+
+Cómo determinar el tipo de dato de una variable con JavaScript:
+
+Se puede usar el operador typeof para obtener el tipo de dato de una variable. Considere el ejemplo que ve en pantalla.
+
+let saludo = "Hola a todos 😎";
+let edad = 21;
+
+console.log(typeof saludo);
+console.log(typeof edad);
+*/
