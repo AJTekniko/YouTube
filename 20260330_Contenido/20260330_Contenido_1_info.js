5feaafad75489846e720aa69bb98530b8b5bf9e6
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-03-30 23:52:39 -0600

    Create 20260330_Contenido_1_info.js

---
+++ 20260330_Contenido/20260330_Contenido_1_info.js
@@ -0,0 +1,12 @@
+/*
+Video: youtube.com/shorts/MUrb2X8s4N4
+
+La convención de nombres para variables en JavaScript:
+
+•Las variables necesitan tener nombres descriptivos y significativos.
+•Los nombres se escriben en camel case, o el estilo de escritura en el que se escriben las palabras sin separación o puntuación, y se usan mayúsculas al principio de cada palabra excepto la primera.
+•Las variables necesitan tener nombres que comienzan con una letra, un guion bajo, o un signo de peso.
+•Las variables necesitan tener nombres sin espacios y sin caracteres especiales, excepto por el guion bajo y el signo de peso.
+•Las variables necesitan tener nombres que no son palabras clave.
+•Se distingue entre mayúsculas y minúsculas. Por ejemplo, si existen nombres de variables con los mismos caracteres, pero son diferentes en cuanto a mayúsculas y minúsculas, los nombres son considerados únicos (edad, Edad, y EDAD serían diferentes variables).
+*/
