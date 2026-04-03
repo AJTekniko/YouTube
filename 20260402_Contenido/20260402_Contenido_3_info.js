6af8bb886ea91364b6fefdb9898322a6d3dca746
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-04-03 01:05:59 -0600

    Create 20260402_Contenido_3_info.js

---
+++ 20260402_Contenido/20260402_Contenido_3_info.js
@@ -0,0 +1,12 @@
+/*
+Video: youtube.com/shorts/_Rc1pzIJrJ4
+
+El método includes en JavaScript:
+
+Este método determina si una subcadena existe en una cadena. Considere el ejemplo que ve en pantalla.
+*/
+
+let cadena = "Hola a todos";
+
+console.log(cadena.includes("Hola"));
+console.log(cadena.includes("Saludos"));
