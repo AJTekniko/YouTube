e726b5bfcd76086c7c720471341a681a0f638bdc
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-04-03 01:04:10 -0600

    Create 20260402_Contenido_3_info.py

---
+++ 20260402_Contenido/20260402_Contenido_3_info.py
@@ -0,0 +1,12 @@
+"""
+Video: youtube.com/shorts/vlnXCNog5Wk
+
+El operador in en Python:
+
+Este operador determina si una subcadena existe en una cadena. Considere el ejemplo que ve en pantalla.
+"""
+
+cadena = "Hola a todos"
+
+print("Hola" in cadena)
+print("Saludos" in cadena)
