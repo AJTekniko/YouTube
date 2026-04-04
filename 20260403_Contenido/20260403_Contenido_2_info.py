267b54d536ac4af0031c19d05450442bf1138c7a
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-04-04 17:57:17 -0600

    Create 20260403_Contenido_2_info.py

---
+++ 20260403_Contenido/20260403_Contenido_2_info.py
@@ -0,0 +1,10 @@
+"""
+Video: youtube.com/shorts/fZV-fVe8SDk
+
+El escape de cadenas en Python:
+
+La diagonal invertida puede ser utilizada cuando existen comillas en la cadena de caracteres. Considere el ejemplo que ve en pantalla.
+"""
+
+mensaje = "Yo les digo, \"Hola a todos\""
+print(mensaje)
