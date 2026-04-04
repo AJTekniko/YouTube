d4f777ab3b4f5e793a8ac4488809a111962752ca
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-04-04 17:59:56 -0600

    Create 20260403_Contenido_2_info.js

---
+++ 20260403_Contenido/20260403_Contenido_2_info.js
@@ -0,0 +1,10 @@
+/*
+Video: youtube.com/shorts/NVr2882bsbQ
+
+El escape de cadenas en JavaScript:
+
+La diagonal invertida puede ser utilizada cuando existen comillas en la cadena de caracteres. Considere el ejemplo que ve en pantalla.
+*/
+
+let mensaje = "Yo les digo, \"Hola a todos\"";
+console.log(mensaje);
