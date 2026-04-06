740476f639a01f837c591b8142bea66054172e7d
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-04-05 23:48:03 -0600

    Create 20260405_Contenido_1_info.py

---
+++ 20260405_Contenido/20260405_Contenido_1_info.py
@@ -0,0 +1,5 @@
+"""
+Video: youtube.com/shorts/LOK6cR2PpDc
+
+Esta semana, yo subí videos de Python y JavaScript que tratan con la convención de nombres para variables, cómo determinar el tipo de dato de una variable, heterogramas, pangramas, el operador in y el método includes, y el escape de cadenas. Subí videos de CSS que explican cómo activar modo claro y modo oscuro, y diferentes propiedades background, incluyendo el gradiente lineal. También subí videos de Go, de palabras en inglés, y cómo abrir cmd. ¿Cuál fue su video favorito? Por favor escríbalo en los comentarios. Muchas gracias.
+"""
