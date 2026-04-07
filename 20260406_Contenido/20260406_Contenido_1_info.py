1fac9f9e5db518107ef25c5874e0ed314c8a3f38
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-04-07 00:00:47 -0600

    Create 20260406_Contenido_1_info.py

---
+++ 20260406_Contenido/20260406_Contenido_1_info.py
@@ -0,0 +1,12 @@
+"""
+Video: youtube.com/shorts/JLY0ujKn5UY
+
+Cómo convertir una cadena a mayúsculas con Python:
+
+Podemos utilizar el método upper(), que es derivado de uppercase que signfica mayúsculas, para convertir todos los caracteres a mayúsculas. Si queremos que solamente el primer carácter sea mayúscula, usamos el método capitalize(). Y si queremos que cada palabra empiece con una letra mayúscula, usamos el método title(), que signfica título. Considere los ejemplos que ve en pantalla.
+"""
+
+cadena = "hola a todos"
+print(cadena.upper())
+print(cadena.capitalize())
+print(cadena.title())
