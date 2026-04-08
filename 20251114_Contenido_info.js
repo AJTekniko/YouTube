7c9ef6f31a036fb2f79ca63342809c5493570a43
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-04-07 23:41:00 -0600

    Add video link

--- 20251114_Contenido_info.js
+++ 20251114_Contenido_info.js
@@ -1,4 +1,6 @@
 /*
+Video: youtube.com/watch?v=9tTwoiYeuqk
+
 Cómo producir la cantidad de días que faltan para el fin de semana en JavaScript:
 
 Para empezar, podemos crear una función personalizada que acepta la fecha como cadena de caracteres. Creamos una variable para almacenar la cadena en forma de fecha y con los guiones reemplazados por diagonales. En otra variable, le restamos la fecha en forma de día númerico a seis que representa sábado. Usamos sentencias condicionales para retornar el mensaje apropiado. Después simplemente lo mandamos a la consola.
