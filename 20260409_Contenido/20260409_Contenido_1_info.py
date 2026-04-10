c442e40c79e8b4b8b31050b0d0e610c04e145d83
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-04-09 20:34:03 -0600

    Create 20260409_Contenido_1_info.py

---
+++ 20260409_Contenido/20260409_Contenido_1_info.py
@@ -0,0 +1,10 @@
+"""
+Video: youtube.com/shorts/QBYR5xJhacg
+
+Cómo convertir una cadena a minúsculas con Python:
+
+Podemos utilizar el método lower(), que es derivado de lowercase que signfica minúsculas, para convertir todos los caracteres a minúsculas. Considere el ejemplo que ve en pantalla.
+"""
+
+cadena = "HOLA A TODOS"
+print(cadena.lower())
