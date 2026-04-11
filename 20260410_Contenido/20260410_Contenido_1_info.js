d244a7c19465a0e8dd60aa10bc03f338657e117a
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-04-10 23:50:49 -0600

    Create 20260410_Contenido_1_info.js

---
+++ 20260410_Contenido/20260410_Contenido_1_info.js
@@ -0,0 +1,10 @@
+/*
+Video: youtube.com/shorts/d9s3SQ1xAJU
+
+Cómo convertir una cadena a minúsculas con JavaScript:
+
+Podemos utilizar el método toLowerCase(), que signfica minúsculas, para convertir todos los caracteres a minúsculas. Considere el ejemplo que ve en pantalla.
+*/
+
+let cadena = "HOLA A TODOS";
+console.log(cadena.toLowerCase());
