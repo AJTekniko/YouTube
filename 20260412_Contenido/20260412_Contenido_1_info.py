7670aeaa94d7cecb6fde549cb9f6bee4ce89b65e
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-04-12 20:33:24 -0600

    Create 20260412_Contenido_1_info.py

---
+++ 20260412_Contenido/20260412_Contenido_1_info.py
@@ -0,0 +1,5 @@
+"""
+Video: youtube.com/shorts/GUokXeFVsN4
+
+Esta semana, yo subí videos de Python y JavaScript que tratan con las mayúsculas y las minúsculas. Subí un video de CSS que habla sobre el gradiente radial. Subí videos de Go que explican cómo convertir un número binario en un número decimal y viceversa. También subí varios videos con palabras en inglés para principiantes. ¿Cuál fue su video favorito? Por favor escríbalo en los comentarios. Muchas gracias.
+"""
