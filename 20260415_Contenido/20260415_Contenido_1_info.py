a5b86cdd0b11773fc0164161ce53a200a5535968
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-04-15 19:58:05 -0600

    Create 20260415_Contenido_1_info.py

---
+++ 20260415_Contenido/20260415_Contenido_1_info.py
@@ -0,0 +1,11 @@
+"""
+Video: youtube.com/shorts/XRCVEatafNw
+
+El método replace en Python:
+
+Este método crea una nueva cadena en la que reemplaza una subcadena con otra. Por medio del tercer argumento, se puede especificar la cantidad de veces que necesita ser reemplazada. Si el tercer argumento es omitido, entonces todos los instantes en los que se encuentra la subcadena indicada son reemplazados por otra. Considere el ejemplo que ve en pantalla.
+"""
+
+cadena = "Mi perro es un perro que se llama 'perro'."
+
+print(cadena.replace("perro", "mascota", 1))
