993eb0944c3adbf28e097590db43818b8cb3abbf
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-04-16 20:36:59 -0600

    Create 20260416_Contenido_1_info.js

---
+++ 20260416_Contenido/20260416_Contenido_1_info.js
@@ -0,0 +1,11 @@
+/*
+Video: youtube.com/shorts/sb77KdbxRgQ
+
+El método replace en JavaScript:
+
+Este método crea una nueva cadena en la que reemplaza una subcadena con otra. Considere el ejemplo que ve en pantalla.
+*/
+
+let cadena = "Mi perro es un perro que se llama 'perro'.";
+
+console.log(cadena.replace("perro", "mascota"));
