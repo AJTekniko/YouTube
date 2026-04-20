3494e440915503b1b23473ccdb3bd27949b132f6
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-04-19 20:23:11 -0600

    Create 20260419_Contenido_1_info.py

---
+++ 20260419_Contenido/20260419_Contenido_1_info.py
@@ -0,0 +1,5 @@
+"""
+Video: youtube.com/shorts/W_V8S1zTKyg
+
+Esta semana, hablé de las propiedades border y outline and CSS, del método replace() en Python y JavaScript, y expliqué cómo asignar valores condicionales a una variable en Go. También subí varios videos con palabras en inglés para principiantes. ¿Cuál fue su video favorito? Por favor escríbalo en los comentarios. Muchas gracias.
+"""
