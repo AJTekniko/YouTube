34beac2f2510176d7fcd54cd567b8ad5ae77b5b1
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-04-21 18:28:52 -0600

    Create 20260421_Contenido_1_info.py

---
+++ 20260421_Contenido/20260421_Contenido_1_info.py
@@ -0,0 +1,12 @@
+"""
+Video: youtube.com/shorts/GJ2HRXotGcY
+
+El método split en Python:
+
+Este método es utilizado para dividir una cadena en una lista de subcadenas basado en un separador especificado. Si el separador es omitido, la separación ocurre en donde haya espacio blanco. También se puede indicar cuántas divisiones necesita hacer. Considere el ejemplo que ve en pantalla.
+"""
+
+frase = "hola-a-todos"
+frase_dividida = frase.split("-", 1)
+
+print(frase_dividida)
