c8725030eb2c9cd5c85d7fd86f45fc00eb09980f
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-04-22 20:27:29 -0600

    Create 20260422_Contenido_1_info.js

---
+++ 20260422_Contenido/20260422_Contenido_1_info.js
@@ -0,0 +1,12 @@
+/*
+Video: youtube.com/shorts/t878qcic1Ck
+
+El método split en JavaScript:
+
+Este método es utilizado para dividir una cadena en una matriz de subcadenas basado en un separador especificado. Si el separador es omitido, retorna una matriz que contiene la cadena entera. También se puede indicar cuántas subcadenas queremos en la matriz. Considere el ejemplo que ve en pantalla.
+*/
+
+let frase = "hola-a-todos";
+let frase_dividida = frase.split("-", 1);
+
+console.log(frase_dividida);
