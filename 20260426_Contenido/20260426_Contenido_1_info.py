3d277bbf1f8590ed63e95761476b42ebc6221916
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-04-26 23:43:24 -0600

    Create 20260426_Contenido_1_info.py

---
+++ 20260426_Contenido/20260426_Contenido_1_info.py
@@ -0,0 +1,5 @@
+"""
+Video: youtube.com/shorts/DP643J0ontg
+
+Esta semana, hablé de valores posicionales en Go, del método split() en Python y JavaScript, y de la propiedad border-radius en CSS. ¿Cuál fue su video favorito? Por favor escríbalo en los comentarios. Muchas gracias.
+"""
