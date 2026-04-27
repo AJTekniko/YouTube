04ab3d296046e9a3307836e18629ae1939c26e1f
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-04-27 13:42:01 -0600

    Create 20260427_Contenido_1_info.py

---
+++ 20260427_Contenido/20260427_Contenido_1_info.py
@@ -0,0 +1,12 @@
+"""
+Video: youtube.com/shorts/AFqKwYFLZ_o
+
+El método join en Python:
+
+Este método es utilizado para unir los elementos de un iterable y convertirlos en una cadena con un separador. Considere el ejemplo que ve en pantalla.
+"""
+
+lista = ["hola", "a", "todos"]
+cadena = " ".join(lista)
+
+print(cadena)
