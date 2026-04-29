deee09aeef5214273c61c40a1763b1ec7debc83e
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-04-28 18:21:02 -0600

    Create 20260428_Contenido_1_info.js

---
+++ 20260428_Contenido/20260428_Contenido_1_info.js
@@ -0,0 +1,18 @@
+/*
+Video: youtube.com/shorts/hVjMi4_mJjE
+
+El método join en JavaScript:
+
+Este método es utilizado para unir los elementos de un iterable y convertirlos en una cadena con un separador. Si el separador es omitido, los elementos de la matriz son separados con una coma. Considere los ejemplos que ve en pantalla.
+*/
+
+let matriz = ["hola", "a", "todos"];
+let cadena = matriz.join(" ");
+
+console.log(cadena);
+
+
+let matriz = ["hola", "a", "todos"];
+let cadena = matriz.join();
+
+console.log(cadena);
