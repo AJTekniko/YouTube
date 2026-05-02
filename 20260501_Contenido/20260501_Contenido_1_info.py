2a25dc64610d835fb5f5e70071a5e30bebc68df8
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-05-01 19:22:37 -0600

    Create 20260501_Contenido_1_info.py

---
+++ 20260501_Contenido/20260501_Contenido_1_info.py
@@ -0,0 +1,10 @@
+"""
+Video: youtube.com/shorts/vaYU2jTaHyM
+
+El método startswith en Python:
+
+Este método indica si una cadena empieza con un prefijo en específico por medio de un booleano. El prefijo puede ser una tupla. Se pueden utilizar parámetros opcionales para que el método empiece y termine en ciertas posiciones de la cadena. Considere el ejemplo que ve en pantalla.
+"""
+
+programador = 'AJTekniko'
+print(programador.startswith('AJ'))
