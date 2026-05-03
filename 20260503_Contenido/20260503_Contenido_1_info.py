29a2e3308ca8b3b7a9d82a3f1802ef96b036026d
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-05-03 16:55:50 -0600

    Create 20260503_Contenido_1_info.py

---
+++ 20260503_Contenido/20260503_Contenido_1_info.py
@@ -0,0 +1,5 @@
+"""
+Video: youtube.com/shorts/KgiIljw7IZQ
+
+Esta semana, hablé del método join() en Python y JavaScript, de la especificidad en CSS, de la suma de números proporcionados en Go, y del método startswith() en Python. Muchas gracias por su apoyo.
+"""
