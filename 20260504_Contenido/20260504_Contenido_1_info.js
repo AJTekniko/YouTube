fd08c2e61e2808847a6679364b21d34bb9c370ad
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-05-04 13:18:45 -0600

    Create 20260504_Contenido_1_info.js

---
+++ 20260504_Contenido/20260504_Contenido_1_info.js
@@ -0,0 +1,10 @@
+/*
+Video: youtube.com/shorts/xpU4pA7NpxY
+
+El método startsWith en JavaScript:
+
+Este método indica si una cadena empieza con ciertos caracteres por medio de un booleano. Se puede utilizar un parámetro opcional para indicar dónde debería de empezar el método. Considere el ejemplo que ve en pantalla.
+*/
+
+let programador = 'AJTekniko';
+console.log(programador.startsWith('AJ'));
