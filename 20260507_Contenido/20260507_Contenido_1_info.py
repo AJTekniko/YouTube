1a6b00eead4dd0949b6bdde1c195df55d3627916
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-05-07 17:46:44 -0600

    Create 20260507_Contenido_1_info.py

---
+++ 20260507_Contenido/20260507_Contenido_1_info.py
@@ -0,0 +1,10 @@
+"""
+Video: youtube.com/shorts/0nV_QNye0F0
+
+El método endswith en Python:
+
+Este método indica si una cadena termina con un sufijo en específico por medio de un booleano. El sufijo puede ser una tupla. Se pueden utilizar parámetros opcionales para que el método empiece y termine en cierta posición de la cadena. Considere el ejemplo que ve en pantalla.
+"""
+
+programador = 'AJTekniko'
+print(programador.endswith('Tekniko'))
