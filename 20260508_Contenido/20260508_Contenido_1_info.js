65b0d7fedfc507b247fdc45c9fa50c23fa19fda6
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-05-08 20:29:55 -0600

    Create 20260508_Contenido_1_info.js

---
+++ 20260508_Contenido/20260508_Contenido_1_info.js
@@ -0,0 +1,10 @@
+/*
+Video: youtube.com/shorts/7Gxje6vEXjM
+
+El método endsWith en JavaScript:
+
+Este método indica si una cadena termina con ciertos caracteres por medio de un booleano. Se puede utilizar un parámetro opcional para indicar en dónde debería de parar el método. Considere el ejemplo que ve en pantalla.
+*/
+
+let programador = 'AJTekniko';
+console.log(programador.endsWith('Tekniko'));
