b3f7e5a0ee77ef2b5f9eccb5c81da82f5377bd13
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-05-10 23:45:17 -0600

    Create 20260510_Contenido_1_info.py

---
+++ 20260510_Contenido/20260510_Contenido_1_info.py
@@ -0,0 +1,5 @@
+"""
+Video: youtube.com/shorts/zJg4NI6RY4I
+
+Esta semana, hablé del método startsWith() en JavaScript, de unidades absolutas en CSS, de la raíz cuadrada en Go, y del método endswith() y endsWith(), con la w mayúscula, en Python y JavaScript. Muchas gracias por su apoyo.
+"""
