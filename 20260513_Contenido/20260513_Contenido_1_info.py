dcc27c0c95a88dcdc9728ea23bd2d13ada5a18c3
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-05-13 23:29:18 -0600

    Create 20260513_Contenido_1_info.py

---
+++ 20260513_Contenido/20260513_Contenido_1_info.py
@@ -0,0 +1,13 @@
+"""
+Video: youtube.com/shorts/peZwqp7C2Rw
+
+El método find en Python:
+
+Este método retorna el índice de la subcadena indicada. También se puede indicar el comienzo y el final del método por medio de dos argumentos opcionales. Si la subcadena no se encuentra, retorna -1. Considere los ejemplos que ve en pantalla.
+"""
+
+programador = 'AJTekniko'
+print(programador.find('A'))
+
+ciudad = 'Los Angeles'
+print(ciudad.find('New'))
