1be20477e3eb7878c9b6e81dcc4ba4e368e3a918
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-05-14 17:35:54 -0600

    Create 20260514_Contenido_1_info.js

---
+++ 20260514_Contenido/20260514_Contenido_1_info.js
@@ -0,0 +1,13 @@
+/*
+Video: youtube.com/shorts/Q-Q2iccntRU
+
+El método indexOf en JavaScript:
+
+Este método retorna el índice de la subcadena indicada. También se puede indicar el comienzo del método por medio de un argumento opcional. Si la subcadena no se encuentra, retorna -1. Considere los ejemplos que ve en pantalla.
+*/
+
+let programador = 'AJTekniko';
+console.log(programador.indexOf('A'));
+
+let ciudad = 'Los Angeles';
+console.log(ciudad.indexOf('New'));
