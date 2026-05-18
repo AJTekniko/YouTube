dc756b222b6137cc896d7d7510ecd63980390d90
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-05-17 20:29:08 -0600

    Create 20260517_Contenido_1_info.py

---
+++ 20260517_Contenido/20260517_Contenido_1_info.py
@@ -0,0 +1,5 @@
+"""
+Video: youtube.com/shorts/nC3EqLrq0GI
+
+Esta semana, hablé del intercambio de variables en Go, de las unidades relativas en CSS, del método find() en Python, del método indexOf() en JavaScript, y de la función calc() en CSS. Muchas gracias por su apoyo.
+"""
