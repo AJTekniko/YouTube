5be8bf414ff2cc14b7111e27bb9dc5b6a612d20f
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-05-19 20:06:34 -0600

    Create 20260519_Contenido_1_info.py

---
+++ 20260519_Contenido/20260519_Contenido_1_info.py
@@ -0,0 +1,10 @@
+"""
+Video: youtube.com/shorts/bgJjyukmZoM
+
+El método count en Python:
+
+Este método retorna la cantidad de veces que un valor en específico se encuentra en una secuencia, como una lista, una cadena o una tupla. Cuando el método es aplicado a una cadena, se puede indicar el comienzo y el final del método por medio de dos argumentos opcionales. Si la subcadena no se encuentra, retorna 0. Considere el ejemplo que ve en pantalla.
+"""
+
+palabra = 'programación'
+print(palabra.count('a'))
