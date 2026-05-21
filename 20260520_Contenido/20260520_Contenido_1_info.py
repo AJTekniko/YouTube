00b5622c965df572701d47f77acb59e3ef12fb42
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-05-20 20:10:50 -0600

    Create 20260520_Contenido_1_info.py

---
+++ 20260520_Contenido/20260520_Contenido_1_info.py
@@ -0,0 +1,10 @@
+"""
+Video: youtube.com/shorts/U7y42U2xlE4
+
+El método isupper en Python:
+
+Este método retorna el valor True si todas las letras en una cadena de caracteres son mayúsculas y existe por lo menos una letra en la cadena. De otro modo, retorna el valor False. Considere el ejemplo que ve en pantalla.
+"""
+
+programador = 'ajtekniko'
+print(programador.isupper())
