3dd63803d5d8a94fe827dd92354ed58c3d3c1a6b
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-05-24 23:38:43 -0600

    Create 20260524_Contenido_1_info.py

---
+++ 20260524_Contenido/20260524_Contenido_1_info.py
@@ -0,0 +1,5 @@
+"""
+Video: youtube.com/shorts/P1QeV0fxyug
+
+Esta semana, hablé de números aleatorios en Go, de los métodos count() e isupper() en Python, de los colores nombrados en CSS, y de la conversión de kilómetros y millas en Go. También subí una pregunta de entrevista con la respuesta en JavaScript, la cual trata con intercambiar mayúsculas y minúsculas. Muchas gracias por su apoyo.
+"""
