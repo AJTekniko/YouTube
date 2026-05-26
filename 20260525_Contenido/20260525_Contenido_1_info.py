a4474806e6303f1d0f7b030039bd359fa780ea46
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-05-25 19:59:43 -0600

    Create 20260525_Contenido_1_info.py

---
+++ 20260525_Contenido/20260525_Contenido_1_info.py
@@ -0,0 +1,10 @@
+"""
+Video: youtube.com/shorts/B_txxiNAYe0
+
+El método islower en Python:
+
+Este método retorna el valor True si todas las letras en una cadena de caracteres son minúsculas y existe por lo menos una letra en la cadena. De otro modo, retorna el valor False. Considere el ejemplo que ve en pantalla.
+"""
+
+programador = 'ajtekniko'
+print(programador.islower())
