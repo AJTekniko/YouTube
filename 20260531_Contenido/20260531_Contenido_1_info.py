47aab77f1e148e99c16839c7d3ac4d31c20d2e63
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-05-31 23:49:15 -0600

    Create 20260531_Contenido_1_info.py

---
+++ 20260531_Contenido/20260531_Contenido_1_info.py
@@ -0,0 +1,5 @@
+"""
+Video: youtube.com/shorts/N9DTxMr7YCs
+
+Esta semana, hablé del método islower() en Python, de las funciones rgb() y rgba() en CSS, de la conversión de temperaturas en Go, y de los métodos maketrans y translate en Python. También subí una pregunta de entrevista con la respuesta en JavaScript, la cual trata con mover a ciertos clientes de una línea hasta el final. Muchas gracias por su apoyo.
+"""
