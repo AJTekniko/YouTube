b6fdd210fb787b6dde01492a3c3c25e46182296c
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-06-01 20:37:14 -0600

    Create 20260601_Contenido_1_info.py

---
+++ 20260601_Contenido/20260601_Contenido_1_info.py
@@ -0,0 +1,11 @@
+"""
+Video: youtube.com/shorts/xDZuMfm_zr8
+
+La función float en Python:
+
+Esta función es utilizada para convertir un número entero o cadena de caracteres a flotante. Considere el ejemplo que ve en pantalla.
+"""
+
+num = 7
+
+print(float(num))
