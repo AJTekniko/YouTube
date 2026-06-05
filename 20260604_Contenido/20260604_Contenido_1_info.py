b589203aebb9233a88622e9cba024e26b883d824
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-06-04 23:40:36 -0600

    Create 20260604_Contenido_1_info.py

---
+++ 20260604_Contenido/20260604_Contenido_1_info.py
@@ -0,0 +1,11 @@
+"""
+Video: youtube.com/shorts/C69P0r2riGw
+
+La función int en Python:
+
+Esta función es utilizada para convertir un número flotante o cadena de caracteres a número entero. Un segundo parámetro se puede utilizar para indicar la base. La base predeterminada es 10. Considere el ejemplo que ve en pantalla.
+"""
+
+num = 4.0
+
+print(int(num))
