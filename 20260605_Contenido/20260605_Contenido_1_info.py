5b613541b2e7ddf80ce0379e8b80a282601e67ab
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-06-05 23:36:54 -0600

    Create 20260605_Contenido_1_info.py

---
+++ 20260605_Contenido/20260605_Contenido_1_info.py
@@ -0,0 +1,13 @@
+"""
+Video: youtube.com/shorts/2SmMovdq3zk
+
+La función round en Python:
+
+Esta función es utilizada para redondear un número. Un segundo parámetro se puede utilizar para indicar la precisión. La precisión predeterminada es el número entero más cercano. Considere el ejemplo que ve en pantalla.
+"""
+
+num_1 = 14
+num_2 = 7.7
+
+print(round(num_1, -1))
+print(round(num_2))
