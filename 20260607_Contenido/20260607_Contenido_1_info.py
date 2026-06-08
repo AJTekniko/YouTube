8a899decb92eb80373e81a59470623f35912a033
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-06-07 20:07:46 -0600

    Create 20260607_Contenido_1_info.py

---
+++ 20260607_Contenido/20260607_Contenido_1_info.py
@@ -0,0 +1,5 @@
+"""
+Video: youtube.com/shorts/W3OjkCvwnjg
+
+Esta semana, hablé de la función float() en Python, de la función hsl() en CSS, de números en Go para saber si son positivos, negativos, o cero, y de las funciones int() y round() en Python. Muchas gracias por su apoyo.
+"""
