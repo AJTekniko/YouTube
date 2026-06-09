7deb94217018fdc916ca2129598c571e51e4d9a7
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-06-08 21:01:30 -0600

    Create 20260608_Contenido_1_info.js

---
+++ 20260608_Contenido/20260608_Contenido_1_info.js
@@ -0,0 +1,13 @@
+/*
+Video: youtube.com/shorts/FyuO3QUN6tE
+
+La función round en JavaScript:
+
+Esta función es utilizada para redondear un número. Considere el ejemplo que ve en pantalla.
+*/
+
+num1 = 5.5
+num2 = -5.5
+
+console.log(Math.round(num1));
+console.log(Math.round(num2));
