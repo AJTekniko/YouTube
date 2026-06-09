b3e3e3c7a3e2636c29137586fb87e3cd078af99e
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-06-08 21:04:29 -0600

    Create 20260608_Contenido_2_info.py

---
+++ 20260608_Contenido/20260608_Contenido_2_info.py
@@ -0,0 +1,11 @@
+"""
+Video: youtube.com/shorts/O_1b6RmdSoU
+
+La función abs en Python:
+
+Esta función es utilizada para obtener el valor absoluto de un número. Considere el ejemplo que ve en pantalla.
+"""
+
+num = -7
+
+print(abs(num))
