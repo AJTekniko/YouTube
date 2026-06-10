efad33f8db250d189f9b204dcc9c53318105c14b
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-06-09 23:59:34 -0600

    Create 20260609_Contenido_2_info.py

---
+++ 20260609_Contenido/20260609_Contenido_2_info.py
@@ -0,0 +1,10 @@
+"""Video: youtube.com/shorts/brxw49tHbk4
+
+La función pow en Python:
+
+Esta función es utilizada para obtener el valor de una base elevada al exponente. Considere el ejemplo que ve en pantalla.
+"""
+
+resultado = pow(2, 3)
+
+print(resultado)
