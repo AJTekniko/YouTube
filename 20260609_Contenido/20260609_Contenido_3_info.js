e797047c32f750e5b92d845c092c4a562cc47e08
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-06-10 00:06:38 -0600

    Add files via upload

---
+++ 20260609_Contenido/20260609_Contenido_3_info.js
@@ -0,0 +1,28 @@
+/*
+Video: youtube.com/shorts/-FZoR74bJxI
+
+Las asignaciones aumentadas en JavaScript:
+
+Estas asignaciones le pueden aplicar una operación aritmética a una variable y almacenar el resultado en la misma variable. Considere los ejemplos que ve en pantalla.
+*/
+
+let x = 10;
+console.log(`El valor original de x es ${x}`);
+
+x += 5;
+console.log(`x += 5 produce ${x}`);
+
+x -= 3;
+console.log(`x -= 3 produce ${x}`);
+
+x *= 2;
+console.log(`x *= 2 produce ${x}`);
+
+x /= 3;
+console.log(`x /= 3 produce ${x}`);
+
+x %= 3;
+console.log(`x %= 3 produce ${x}`);
+
+x **= 4;
+console.log(`x **= 4 produce ${x}`);
---
+++ 20260609_Contenido/20260609_Contenido_3_info.py
@@ -0,0 +1,28 @@
+"""
+Video: youtube.com/shorts/dXzb8Iu3-vw
+
+Las asignaciones aumentadas en Python:
+
+Estas asignaciones le pueden aplicar una operación aritmética a una variable y almacenar el resultado en la misma variable. Considere los ejemplos que ve en pantalla.
+"""
+
+x = 10
+print(f"El valor original de x es {x}")
+
+x += 5
+print(f"x += 5 produce {x}")
+
+x -= 3
+print(f"x -= 3 produce {x}")
+
+x *= 2
+print(f"x *= 2 produce {x}")
+
+x /= 3
+print(f"x /= 3 produce {x}")
+
+x %= 3
+print(f"x %= 3 produce {x}")
+
+x **= 4
+print(f"x **= 4 produce {x}")
