279c17397c7bf7c18edf62c584881c7cb919fe50
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-06-10 00:00:31 -0600

    Create 20260609_Contenido_2_info.js

---
+++ 20260609_Contenido/20260609_Contenido_2_info.js
@@ -0,0 +1,11 @@
+/*
+Video: youtube.com/shorts/2bb63Rfyku8
+
+El método pow en JavaScript:
+
+Este método es utilizado para obtener el valor de una base elevada al exponente. Considere el ejemplo que ve en pantalla.
+*/
+
+resultado = Math.pow(2, 3);
+
+console.log(resultado);
