0e5a0536ee78e1c7f07dae6ec24b27131cdaf8c9
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-06-11 23:33:49 -0600

    Create 20260611_Contenido_2_info.py

---
+++ 20260611_Contenido/20260611_Contenido_2_info.py
@@ -0,0 +1,12 @@
+"""
+Video: youtube.com/shorts/ldpdmhLY280
+
+TypeError en Python:
+
+Este error puede ocurrir cuando un operando o argumento es incompatible con el tipo que es anticipado, cuando un valor que no puede ser alterado se intenta alterar, o al intentar de utilizar un valor de manera inapropiada. Considere el ejemplo que ve en pantalla.
+"""
+
+def sumar(a, b):
+    print(a + b)
+
+sumar()
