fead98a2bb96f53ba0e81983797b3b9718ff17e5
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-06-11 23:35:47 -0600

    Create 20260611_Contenido_2_info.js

---
+++ 20260611_Contenido/20260611_Contenido_2_info.js
@@ -0,0 +1,10 @@
+/*
+Video: youtube.com/shorts/2UyryuwI3Ck
+
+TypeError en JavaScript:
+
+Este error puede ocurrir cuando un operando o argumento es incompatible con el tipo que es anticipado, cuando un valor que no puede ser alterado se intenta alterar, o al intentar de utilizar un valor de manera inapropiada. Considere el ejemplo que ve en pantalla.
+*/
+
+let cuenta = 42;
+cuenta();
