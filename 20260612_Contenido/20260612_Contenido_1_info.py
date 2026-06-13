763d45d43eebc78af2f3e0b5eea48993430c8533
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-06-12 23:36:18 -0600

    Create 20260612_Contenido_1_info.py

---
+++ 20260612_Contenido/20260612_Contenido_1_info.py
@@ -0,0 +1,13 @@
+"""
+Video: youtube.com/shorts/dFsPHlpXN9U
+
+El ámbito local en Python:
+
+Una variable que es declarada dentro de una función o clase solo es accesible en esa parte del programa. Considere el ejemplo que ve en pantalla.
+"""
+
+def mi_func():
+    num = 7
+    print(num)
+
+print(num)
