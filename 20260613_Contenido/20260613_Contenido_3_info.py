9b130705b4557b5e090d93b65ad053c09ce3a7b1
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-06-13 23:37:09 -0600

    Create 20260613_Contenido_3_info.py

---
+++ 20260613_Contenido/20260613_Contenido_3_info.py
@@ -0,0 +1,11 @@
+"""
+Video: youtube.com/shorts/WgOdSasI5mo
+
+Cómo importar un módulo con un alias en Python:
+
+Podemos importar un módulo con un nombre diferente utilizando la palabra clave as. Esta palabra clave también puede ser utilizada para importar elementos de un módulo e implementarlos en nuestro programa con nombres diferentes. Considere el ejemplo que ve en pantalla.
+"""
+
+import mi_modulo as mi_alias
+
+print(mi_alias.saludar("compañeros"), "(ej. alias)")
