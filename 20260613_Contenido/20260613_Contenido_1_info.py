d16cedbc3eb8ba74a2e0e5aee0e02d0e0877a089
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-06-13 23:33:59 -0600

    Create 20260613_Contenido_1_info.py

---
+++ 20260613_Contenido/20260613_Contenido_1_info.py
@@ -0,0 +1,15 @@
+"""
+Video: youtube.com/shorts/jvDGkrvmv7w
+
+Cómo crear un archivo que puede funcionar como módulo en Python:
+
+Creamos un archivo de Python con la extensión .py en su nombre y escribimos las funciones necesarias en ese archivo. Considere el ejemplo que ve en pantalla.
+"""
+
+# mi_modulo.py
+
+def saludar(nombre):
+    return f"¡Hola {nombre}!"
+
+def sumar(a, b):
+    return a + b
