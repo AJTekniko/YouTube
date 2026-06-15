99f93e75f2480c177ddb97ec4739b282406de661
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-06-14 23:20:13 -0600

    Create 20260614_Contenido_1_info.py

---
+++ 20260614_Contenido/20260614_Contenido_1_info.py
@@ -0,0 +1,5 @@
+"""
+Video: youtube.com/shorts/hRdvum7jZLI
+
+Esta semana, hablé de la función round en JavaScript, de abs, de pow, de asignaciones aumentadas, de funciones personalizadas, del valor predeterminado de return, del valor predeterminado de parámetros, de TypeError, y del ámbito local en Python y JavaScript. También cubrí el ámbito envolvente y módulos en Python. Muchas gracias por su apoyo.
+"""
