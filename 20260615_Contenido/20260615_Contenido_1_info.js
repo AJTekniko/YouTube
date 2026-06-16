0d7ebc00254aab9c6affb715f9547cd3d1b064ab
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-06-15 22:54:39 -0600

    Create 20260615_Contenido_1_info.js

---
+++ 20260615_Contenido/20260615_Contenido_1_info.js
@@ -0,0 +1,13 @@
+/*
+Video: youtube.com/shorts/Sd-_-P6n6QU
+
+El ámbito de bloque en JavaScript:
+
+Se refiere a la accesibilidad de declaraciones como let y const encerradas entre llaves. Considere el ejemplo que ve en pantalla.
+*/
+
+let x = 1;
+{
+  let x = 2;
+}
+console.log(x);
