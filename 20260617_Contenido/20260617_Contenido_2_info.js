80941904b1b69b27e34fc43890e04655c7d3fe1a
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-06-17 20:40:46 -0600

    Create 20260617_Contenido_2_info.js

---
+++ 20260617_Contenido/20260617_Contenido_2_info.js
@@ -0,0 +1,14 @@
+/*
+Video: youtube.com/shorts/k_PmZUYErZk
+
+Los valores truthy y falsy en JavaScript:
+
+Los valores truthy son aquellos que resultan en true en un contexto lógico, como números distintos a cero y cadenas de caracteres que no están vacías. Los valores falsy son lo contrario y resultan en false, como null, cadenas vacías, 0, etc. Considere los ejemplos que ve en pantalla.
+*/
+
+if (5) {
+  console.log("Un número distinto a cero es truthy");
+}
+if ("Cadena") {
+  console.log("Una cadena con caracteres es truthy");
+}
