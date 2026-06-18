1011568424f9544edb8fd8b20a6c302938abbe19
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-06-17 20:40:01 -0600

    Create 20260617_Contenido_1_info.py

---
+++ 20260617_Contenido/20260617_Contenido_1_info.py
@@ -0,0 +1,12 @@
+"""
+Video: youtube.com/shorts/73INvdVStlo
+
+Los valores truthy y falsy en Python:
+
+Los valores truthy son aquellos que resultan en True en un contexto lógico, como números distintos a cero y cadenas de caracteres que no están vacías. Los valores falsy son lo contrario y resultan en False, como None, cadenas vacías, 0, etc. Considere los ejemplos que ve en pantalla.
+"""
+
+if 5:
+    print("Un número distinto a cero es truthy")
+if "Cadena":
+    print("Una cadena con caracteres es truthy")
