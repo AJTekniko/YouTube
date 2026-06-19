c7963b179b9391755c271bbb23a737594477361b
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-06-18 19:03:34 -0600

    Create 20260618_Contenido_2_info.js

---
+++ 20260618_Contenido/20260618_Contenido_2_info.js
@@ -0,0 +1,14 @@
+/*
+Video: youtube.com/shorts/msdY2EZiRLA
+
+La función Boolean en JavaScript:
+
+Esta función indica si un argumento tiene un valor de true o de false. Considere los ejemplos que ve en pantalla.
+*/
+
+console.log(Boolean(5));
+console.log(Boolean("cadena"));
+
+console.log(Boolean(null));
+console.log(Boolean(""));
+console.log(Boolean(0));
