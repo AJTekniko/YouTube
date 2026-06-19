e54cb91723960617360d1c24e3023a3bf05c113f
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-06-18 19:04:32 -0600

    Create 20260618_Contenido_3_info.py

---
+++ 20260618_Contenido/20260618_Contenido_3_info.py
@@ -0,0 +1,10 @@
+"""
+Video: youtube.com/shorts/pCbOGpqCc0g
+
+IndexError en Python:
+
+Este error ocurre al querer utilizar un índice que está fuera del rango. Considere el ejemplo que ve en pantalla.
+"""
+
+idiomas = ['Español', 'Inglés', 'Francés']
+idiomas[7] = 'Portugués'
