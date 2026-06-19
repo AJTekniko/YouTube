959c0118efc2c30edd086b765ca075a9dea7e70a
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-06-18 19:02:18 -0600

    Create 20260618_Contenido_1_info.py

---
+++ 20260618_Contenido/20260618_Contenido_1_info.py
@@ -0,0 +1,14 @@
+"""
+Video: youtube.com/shorts/lrrceJh2w58
+
+La función bool en Python:
+
+Esta función indica si un argumento tiene un valor de True o de False. Considere los ejemplos que ve en pantalla.
+"""
+
+print(bool(5))
+print(bool("cadena"))
+
+print(bool(None))
+print(bool(""))
+print(bool(0))
