dcd159c140a4f30876cb72c2a3c4e50a43b94ce0
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-06-19 23:08:54 -0600

    Create 20260619_Contenido_1_info.py

---
+++ 20260619_Contenido/20260619_Contenido_1_info.py
@@ -0,0 +1,11 @@
+"""
+Video: youtube.com/shorts/XRfzgZua_zI
+
+El constructor list en Python:
+
+Este constructor puede ser utilizado para crear listas. Considere el ejemplo que ve en pantalla.
+"""
+
+programador = 'AJTekniko'
+
+print(list(programador)) 
