608fac02637b73120b1efb9b101aa65c73eb5660
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-06-19 23:10:36 -0600

    Create 20260619_Contenido_2_info.py

---
+++ 20260619_Contenido/20260619_Contenido_2_info.py
@@ -0,0 +1,11 @@
+"""
+Video: youtube.com/shorts/XrwhcXh1JGA
+
+La palabra clave del en Python:
+
+Esta palabra clave puede ser utilizada para remover elementos de una lista. Considere el ejemplo que ve en pantalla.
+"""
+
+frutas = ['manzana', 'plátano', 'naranja']
+del frutas[1]
+print(frutas)
