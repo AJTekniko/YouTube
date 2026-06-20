d8fa3c9b57bc2a6c8613de0f2ef0c845b129a059
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-06-19 23:12:12 -0600

    Create 20260619_Contenido_3_info.py

---
+++ 20260619_Contenido/20260619_Contenido_3_info.py
@@ -0,0 +1,10 @@
+"""
+Video: youtube.com/shorts/7X5ghZbZqjk
+
+ValueError en Python:
+
+Este error ocurre cuando una operación o función se topa con un argumento que tiene un valor inapropiado. Considere el ejemplo que ve en pantalla.
+"""
+
+frutas = ['manzana', 'plátano', 'naranja']
+roja, amarilla = frutas
