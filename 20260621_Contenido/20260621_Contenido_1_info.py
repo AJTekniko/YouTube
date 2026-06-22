20a085524a946b2ce29a0a274db8e678bbaf7db0
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-06-21 20:51:24 -0600

    Create 20260621_Contenido_1_info.py

---
+++ 20260621_Contenido/20260621_Contenido_1_info.py
@@ -0,0 +1,11 @@
+"""
+Video: youtube.com/shorts/FNLBwTBruTo
+
+El método append en Python:
+
+Este método es utilizado para agregar un elemento al final de una lista. Considere el ejemplo que ve en pantalla.
+"""
+
+letras = ["a", "b", "c"]
+letras.append("d")
+print(letras)
