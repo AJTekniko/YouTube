34d212e75d6cc283839e37990b5989c231309d95
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-06-22 17:26:18 -0600

    Create 20260622_Contenido_1_info.js

---
+++ 20260622_Contenido/20260622_Contenido_1_info.js
@@ -0,0 +1,11 @@
+/*
+Video: youtube.com/shorts/Rvmhg7mL5Po
+
+El método push en JavaScript:
+
+Este método es utilizado para agregar elementos al final de una matriz. Considere el ejemplo que ve en pantalla.
+*/
+
+let letras = ["a", "b", "c"];
+letras.push("d");
+console.log(letras);
