d40c9c623d83f3b9fd4f9895ee16f48a5313afeb
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-06-21 20:52:07 -0600

    Create 20260621_Contenido_2_info.py

---
+++ 20260621_Contenido/20260621_Contenido_2_info.py
@@ -0,0 +1,5 @@
+"""
+Video: youtube.com/shorts/M6mzWx9pKn0
+
+Esta semana, hablé del ámbito de bloque y de RangeError en JavaScript, del ámbito global, los valores truthy y falsy, y la función bool/Boolean en Python y JavaScript, de los códigos hexadecimales y del desbordamiento en CSS, de los números pares e impares en Go, y de IndexError, el constructor list(), la palabra clave del, ValueError, y el método append() en Python. Muchas gracias por su apoyo.
+"""
