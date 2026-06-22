d19c3abf999e303ee9beefdba455c6f502237f6a
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-06-22 17:27:54 -0600

    Create 20260622_Contenido_2_info.py

---
+++ 20260622_Contenido/20260622_Contenido_2_info.py
@@ -0,0 +1,13 @@
+"""
+Video: youtube.com/shorts/bNeAe_Wjt-k
+
+El método extend en Python:
+
+Este método es utilizado para agregar varios elementos al final de una lista. Considere el ejemplo que ve en pantalla.
+"""
+
+letras = ["a", "b", "c"]
+nuevas_letras = ["d", "e"]
+
+letras.extend(nuevas_letras)
+print(letras)
