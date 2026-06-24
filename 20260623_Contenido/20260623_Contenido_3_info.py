c2b1117f3704d8b2ebdebcab2acdc8bac0cb5fdc
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-06-23 20:48:39 -0600

    Create 20260623_Contenido_3_info.py

---
+++ 20260623_Contenido/20260623_Contenido_3_info.py
@@ -0,0 +1,12 @@
+"""
+Video: youtube.com/shorts/VUDcwN1vzYQ
+
+El método remove en Python:
+
+Este método es utilizado para eliminar la primera ocurrencia de un elemento en una lista. Considere el ejemplo que ve en pantalla.
+"""
+
+letras = ["a", "b", "a", "b"]
+letras.remove("a")
+
+print(letras)
