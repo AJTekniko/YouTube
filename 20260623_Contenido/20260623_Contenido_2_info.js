f21131abc0e10451bbe2a3c5cc6391307a03e306
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-06-23 20:19:14 -0600

    Create 20260623_Contenido_2_info.js

---
+++ 20260623_Contenido/20260623_Contenido_2_info.js
@@ -0,0 +1,13 @@
+/*
+Video: youtube.com/shorts/u034Bso0bm4
+
+El método pop en JavaScript:
+
+Este método es utilizado para quitarle el último elemento a una matriz y retornar ese elemento. Considere el ejemplo que ve en pantalla.
+*/
+
+let letras = ["a", "b", "c"];
+let letraElim = letras.pop();
+
+console.log(letraElim);
+console.log(letras);
