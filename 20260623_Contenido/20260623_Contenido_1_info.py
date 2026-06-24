fbdb80c8f701f1ffe1fdcce051c774524abda62d
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-06-23 20:16:21 -0600

    Create 20260623_Contenido_1_info.py

---
+++ 20260623_Contenido/20260623_Contenido_1_info.py
@@ -0,0 +1,12 @@
+"""
+Video: youtube.com/shorts/PO0fP7Yqm2A
+
+El método insert en Python:
+
+Este método es utilizado para insertarle un elemento a una lista. Considere el ejemplo que ve en pantalla.
+"""
+
+letras = ["a", "b", "c"]
+letras.insert(3, "d")
+
+print(letras)
