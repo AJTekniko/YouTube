80d6fdcd04f6dea024c2dce2eca51f6c9ef519e3
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-06-25 16:44:15 -0600

    Create 20260625_Contenido_1_info.py

---
+++ 20260625_Contenido/20260625_Contenido_1_info.py
@@ -0,0 +1,16 @@
+"""
+Video: youtube.com/shorts/c7qqaNUYkRI
+
+El método pop en Python:
+
+Este método es utilizado para quitarle un elemento a una lista y retornar ese elemento. Si ningún índice es especificado, le quita el último elemento a una lista y lo retorna. Considere el ejemplo que ve en pantalla.
+"""
+
+letras = ["a", "b", "c"]
+
+ultima_letra = letras.pop()
+print(ultima_letra)
+
+primera_letra = letras.pop(0)
+print(primera_letra)
+print(letras)
