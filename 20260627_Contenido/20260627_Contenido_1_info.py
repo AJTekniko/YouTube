770fab5e7ada6e764051a05c7331c358f1361aa4
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-06-27 19:46:18 -0600

    Create 20260627_Contenido_1_info.py

---
+++ 20260627_Contenido/20260627_Contenido_1_info.py
@@ -0,0 +1,11 @@
+"""
+Video: youtube.com/shorts/Q56uPJROj9U
+
+El método clear en Python:
+
+Este método es utilizado para eliminar todos los elementos de una lista. Considere el ejemplo que ve en pantalla.
+"""
+
+letras = ["a", "b", "c"]
+letras.clear()
+print(letras)
