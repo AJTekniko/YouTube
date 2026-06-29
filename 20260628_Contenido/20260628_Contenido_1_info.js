d07dcf0053ea2a5ab72f66d8fc5c0c97e514f19a
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-06-28 19:45:38 -0600

    Create 20260628_Contenido_1_info.js

---
+++ 20260628_Contenido/20260628_Contenido_1_info.js
@@ -0,0 +1,14 @@
+/*
+Video: youtube.com/shorts/mS64IDuVCkM
+
+El método filter en JavaScript:
+
+Este método es utilizado para filtrar los elementos de una matriz, y crear una nueva matriz con todos los elementos que cumplen la condición implementada. Considere el ejemplo que ve en pantalla.
+*/
+
+let numeros = [1, 2, 3, 4];
+
+let pares = numeros.filter((numero) =>
+numero % 2 == 0);
+
+console.log(pares)
