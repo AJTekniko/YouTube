a14b51f7bc349cc384de42a0ed6878ebe7594eda
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-06-28 19:47:16 -0600

    Create 20260628_Contenido_2_info.py

---
+++ 20260628_Contenido/20260628_Contenido_2_info.py
@@ -0,0 +1,5 @@
+"""
+Video: youtube.com/shorts/oyKxRs8jotQ
+
+Esta semana, hablé de los métodos push, extend, insert, remove, y clear en Python, de los métodos splice, break, continue, y filter en JavaScript, y del método pop en Python y JavaScript. Muchas gracias por su apoyo.
+"""
