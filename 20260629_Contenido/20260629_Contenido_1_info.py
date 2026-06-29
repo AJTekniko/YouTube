a502d8a5c452b03dec897084ec4c03909e58015e
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-06-29 16:30:27 -0600

    Create 20260629_Contenido_1_info.py

---
+++ 20260629_Contenido/20260629_Contenido_1_info.py
@@ -0,0 +1,14 @@
+"""
+Video: youtube.com/shorts/SOwqM1xue0w
+
+La sentencia break en Python:
+
+Esta sentencia es utilizada para inmediatamente terminar el ciclo en el que se encuentra la sentencia. Considere el ejemplo que ve en pantalla.
+"""
+
+letras = ["a", "b", "c"]
+
+for letra in letras:
+    if letra == "c":
+        break
+    print(letra)
