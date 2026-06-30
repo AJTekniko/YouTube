f139662b7e132b0fceb63c3b80f380d197e8b725
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-06-30 17:09:11 -0600

    Create 20260630_Contenido_1_info.js

---
+++ 20260630_Contenido/20260630_Contenido_1_info.js
@@ -0,0 +1,11 @@
+/*
+Video: youtube.com/shorts/I_8TnvfBhXU
+
+La función flecha en JavaScript:
+
+Esta función es utilizada como una alternativa a la función tradicional, pero es limitada y no se puede utilizar en todas las situaciones. Considere el ejemplo que ve en pantalla.
+*/
+
+let sumar = (a, b) => a + b;
+
+console.log(sumar(1, 2));
