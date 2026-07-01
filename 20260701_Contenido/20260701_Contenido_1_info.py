bd35c2c5265d678522ccd93697c4b67a5f1ab673
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-07-01 17:49:27 -0600

    Create 20260701_Contenido_1_info.py

---
+++ 20260701_Contenido/20260701_Contenido_1_info.py
@@ -0,0 +1,14 @@
+"""
+Video: youtube.com/shorts/yMWaffI55-k
+
+La sentencia continue en Python:
+
+Esta sentencia es utilizada para terminar la iteración actual de un ciclo y continuar con la siguiente iteración. Considere el ejemplo que ve en pantalla.
+"""
+
+letras = ["a", "b", "c"]
+
+for letra in letras:
+    if letra == "b":
+        continue
+    print(letra)
