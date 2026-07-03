5ccece93327fa56b462335e80e630bc505b5cd9f
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-07-02 23:39:07 -0600

    Create 20260702_Contenido_1_info.js

---
+++ 20260702_Contenido/20260702_Contenido_1_info.js
@@ -0,0 +1,12 @@
+/*
+Video: youtube.com/shorts/L8xvSHiNjTo
+
+El método isSubsetOf en JavaScript:
+
+Este método es utilizado con conjuntos para determinar si los elementos indicados pertenecen a un conjunto. Considere el ejemplo que ve en pantalla.
+*/
+
+let pares = new Set([2, 4, 6, 8, 10]);
+let numeros = new Set([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+
+console.log(pares.isSubsetOf(numeros));
