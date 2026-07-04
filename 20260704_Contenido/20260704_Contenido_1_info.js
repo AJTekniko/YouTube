6fa87f0f4b6b506f652eecf9d0589f4c50835466
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-07-04 17:59:56 -0600

    Create 20260704_Contenido_1_info.js

---
+++ 20260704_Contenido/20260704_Contenido_1_info.js
@@ -0,0 +1,13 @@
+/*
+Video: youtube.com/shorts/XHM34JoMQzI
+
+El método isSupersetOf en JavaScript:
+
+Este método es utilizado con conjuntos para determinar si los elementos indicados pertenecen a un conjunto. Considere el ejemplo que ve en pantalla.
+*/
+
+let pares = new Set([2, 4, 6, 8, 10]);
+let numeros = new Set([1, 2, 3, 4, 5,
+6, 7, 8, 9, 10]);
+
+console.log(numeros.isSupersetOf(pares));
