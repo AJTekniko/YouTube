9795e008f601b1a0eb8c92dac3616b8c2536ef87
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-07-03 18:09:56 -0600

    Create 20260703_Contenido_1_info.py

---
+++ 20260703_Contenido/20260703_Contenido_1_info.py
@@ -0,0 +1,10 @@
+"""
+Video: youtube.com/shorts/BNOHUY8soT4
+
+Range en Python:
+
+Este objeto que significa rango es utilizado para crear una secuencia de números enteros. Consiste de tres parámetros: el comienzo, el fin, y el incremento. El fin es requerido, mientras que el comienzo está preconfigurado a un valor de cero, y el incremento a un valor de uno. Considere el ejemplo que ve en pantalla.
+"""
+
+for num in range(2, 10, 2):
+    print(num)
