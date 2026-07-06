bdf4a5b073a4a9e038d05a4a2d3734575712e000
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-07-05 20:16:41 -0600

    Create 20260705_Contenido_2_info.py

---
+++ 20260705_Contenido/20260705_Contenido_2_info.py
@@ -0,0 +1,5 @@
+"""
+Video: youtube.com/shorts/lVGou8-GsVA
+
+Esta semana, hablé de las sentencias break y continue, de range(), y de la función enumerate() en Python. También cubrí la función flecha, y los métodos isSubsetOf() e isSupersetOf() en JavaScript. Muchas gracias por su apoyo.
+"""
