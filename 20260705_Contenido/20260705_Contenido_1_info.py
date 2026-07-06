9269ab7ac2ad508ddac95e9d8ecf3df63ccc754a
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-07-05 20:13:18 -0600

    Create 20260705_Contenido_1_info.py

---
+++ 20260705_Contenido/20260705_Contenido_1_info.py
@@ -0,0 +1,11 @@
+"""
+Video: youtube.com/shorts/nxMMtBnaN2U
+
+La función enumerate en Python:
+
+Esta función es utilizada para retornar un objeto enumerador que indica el índice de cada elemento en una secuencia. La función tiene un segundo parámetro que empieza el conteo en cierto número; si se omite ese argumento, el conteo está preconfigurado a empezar en 0. Considere el ejemplo que ve en pantalla.
+"""
+
+letras = ["a", "b", "c"]
+
+print(list(enumerate(letras, 1)))
