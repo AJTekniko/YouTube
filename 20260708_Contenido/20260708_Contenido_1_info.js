965f0e6747856a471af3c474cee13261c0457a19
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-07-08 19:38:50 -0600

    Create 20260708_Contenido_1_info.js

---
+++ 20260708_Contenido/20260708_Contenido_1_info.js
@@ -0,0 +1,12 @@
+/*
+Video: youtube.com/shorts/OzJiE-cXBSI
+
+El método intersection en JavaScript:
+
+Este método es utilizado con conjuntos para retornar los elementos que se encuentran en ambos conjuntos involucrados. Considere el ejemplo que ve en pantalla.
+*/
+
+let nums1 = new Set([1, 3, 5, 7, 9]);
+let nums2 = new Set([2, 3, 5, 7]);
+
+console.log(...nums1.intersection(nums2));
