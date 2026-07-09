0fba82e83bc0de84eef2cb54d9591ceb83bfb95f
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-07-09 13:34:24 -0600

    Create 20260709_Contenido_1_info.py

---
+++ 20260709_Contenido/20260709_Contenido_1_info.py
@@ -0,0 +1,11 @@
+"""
+Video: youtube.com/shorts/Icl9NHx7Llo
+
+La comprensión de listas en Python:
+
+Es una manera concisa de crear listas. Considere el ejemplo que ve en pantalla.
+"""
+
+pares = [num for num in range(11) if num % 2 == 0]
+
+print(pares)
