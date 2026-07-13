70407b8d86c4e764a837014d7f7706a934c10fc8
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-07-12 20:29:03 -0600

    Create 20260712_Contenido_1_info.py

---
+++ 20260712_Contenido/20260712_Contenido_1_info.py
@@ -0,0 +1,5 @@
+"""
+Video: youtube.com/shorts/rvrryME-zTc
+
+Esta semana, hablé de los métodos union(), intersection(), y difference() en Javascript, y de la función zip(), de la comprensión de listas, y de la función filter() en Python. Muchas gracias por su apoyo.
+"""
