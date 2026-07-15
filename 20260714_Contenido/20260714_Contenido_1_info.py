49b8794dc467643d20459c6ce9bd391ede034265
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-07-14 18:06:12 -0600

    Create 20260714_Contenido_1_info.py

---
+++ 20260714_Contenido/20260714_Contenido_1_info.py
@@ -0,0 +1,11 @@
+"""
+Video: youtube.com/shorts/HS4kYLBSKgU
+
+La función sum en Python:
+
+Esta función suma los elementos de un iterable. También tiene un parámetro opcional que indica el valor inicial. Considere el ejemplo que ve en pantalla.
+"""
+
+nums = [1, 2, 3, 4, 5]
+total = sum(nums, 10)
+print(total)
