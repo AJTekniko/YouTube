4deb25fd2ab977e3506dfd10333d581ce6ba21ae
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-07-15 19:23:37 -0600

    Create 20260715_Contenido_1_info.js

---
+++ 20260715_Contenido/20260715_Contenido_1_info.js
@@ -0,0 +1,12 @@
+/*
+Video: youtube.com/shorts/6RdAY8sAS9A
+
+El operador in en JavaScript:
+
+Este operador es utilizado para determinar si una propiedad se encuentra en el objeto especificado. No es para buscar valores en otros tipos de colecciones; para eso se utiliza el método includes() con matrices y el método has() con conjuntos. Considere el ejemplo que ve en pantalla.
+*/
+
+let letra = { caracter: "a", posicion: "1",
+tipo: "vocal" };
+
+console.log("caracter" in letra);
