aea8af1579fc077d79dca7807388ae312a183563
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-07-18 00:34:46 -0600

    Create 20260718_Contenido_1_info.js

---
+++ 20260718_Contenido/20260718_Contenido_1_info.js
@@ -0,0 +1,12 @@
+/*
+Video: youtube.com/shorts/pU9wpU6Mmlk
+
+SyntaxError en JavaScript:
+
+Este error ocurre cuando no se sigue la sintaxis de la lengua, como cuando a uno se le olvida cerrar los paréntesis. Considere el ejemplo que ve en pantalla.
+*/
+
+let num = 2;
+if (num % 2 == 0 {
+  console.log("par");
+}
