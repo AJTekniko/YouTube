027128aac1256084b00f7e24b6174ad527e4d78a
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-07-19 20:50:54 -0600

    Create 20260719_Contenido_1_info.py

---
+++ 20260719_Contenido/20260719_Contenido_1_info.py
@@ -0,0 +1,5 @@
+"""
+Video: youtube.com/shorts/rExtGYzKVfk
+
+Esta semana, hablé del método symmetricDifference(), del operador in, y de SyntaxError en JavaScript, y de la función sum() y de la expresión lambda en Python. Muchas gracias por su apoyo.
+"""
