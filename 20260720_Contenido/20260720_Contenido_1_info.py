894bc8652d262732d50b01e30b2192377dcd6778
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-07-20 18:06:42 -0600

    Create 20260720_Contenido_1_info.py

---
+++ 20260720_Contenido/20260720_Contenido_1_info.py
@@ -0,0 +1,11 @@
+"""
+Video: youtube.com/shorts/VZa6ofRS07Y
+
+El constructor dict en Python:
+
+Este constructor permite crear un diccionario por medio de secuencias; utiliza el primer elemento como la clave y el segundo elemento como el valor. Considere el ejemplo que ve en pantalla.
+"""
+
+letras = dict([(1, "a"), (2, "b"), (3, "c")])
+
+print(letras)
