e768be19c908e8b53546120c88cf08d3ed20ede1
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-07-21 15:53:20 -0600

    Create 20260721_Contenido_1_info.js

---
+++ 20260721_Contenido/20260721_Contenido_1_info.js
@@ -0,0 +1,9 @@
+/*
+Video: youtube.com/shorts/WpJsz7llDbw
+
+ReferenceError en JavaScript:
+
+Este error, que significa error de referencia, ocurre al intentar de acceder una variable o función que todavía no ha sido declarada. Considere el ejemplo que ve en pantalla.
+*/
+
+console.log(num)
