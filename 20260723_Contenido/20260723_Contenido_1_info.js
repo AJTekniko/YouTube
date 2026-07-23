00901a5e614d305909639fa9edd9f45832b37279
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-07-23 13:17:38 -0600

    Create 20260723_Contenido_1_info.js

---
+++ 20260723_Contenido/20260723_Contenido_1_info.js
@@ -0,0 +1,10 @@
+/*
+Video: youtube.com/shorts/6BGGJ2kOoaU
+
+TypeError en JavaScript:
+
+Este error, que significa error de tipo, ocurre cuando una operación no puede ser completada porque un valor no es del tipo esperado. Considere el ejemplo que ve en pantalla.
+*/
+
+const letra = { num: "a" };
+letra.num();
