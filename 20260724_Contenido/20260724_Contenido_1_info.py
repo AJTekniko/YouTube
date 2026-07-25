b0326e3c8afcdc19e9546097dda0427d59ec44f1
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-07-24 19:40:26 -0600

    Create 20260724_Contenido_1_info.py

---
+++ 20260724_Contenido/20260724_Contenido_1_info.py
@@ -0,0 +1,12 @@
+"""
+Video: youtube.com/shorts/FIUdJYXzqkM
+
+El método keys en Python:
+
+Este método retorna un objeto vista con las claves del diccionario al que se le aplica. Considere el ejemplo que ve en pantalla.
+"""
+
+letras = dict([(1, "a"),
+(2, "b"), (3, "c")])
+
+print(letras.keys())
