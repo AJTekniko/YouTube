59bca976263a4a23656242a0746ad6e809c9b53b
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-07-26 17:03:21 -0600

    Create 20260726_Contenido_1_info.py

---
+++ 20260726_Contenido/20260726_Contenido_1_info.py
@@ -0,0 +1,5 @@
+"""
+Video: youtube.com/shorts/0Z_0jR5yDs4
+
+Esta semana, hablé del constructor dict(), del método get(), y del método keys() en Python, y de ReferenceError y TypeError en JavaScript. Muchas gracias por su apoyo.
+"""
