1b0208a7965898533c69854a5bb63e89ad7eb8f7
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-07-27 15:27:05 -0600

    Create 20260727_Contenido_1_info.js

---
+++ 20260727_Contenido/20260727_Contenido_1_info.js
@@ -0,0 +1,13 @@
+/*
+Video: youtube.com/shorts/dPCcKoq4dyg
+
+La declaración try...catch en JavaScript:
+
+Esta declaración, que significa intentar y atrapar, es utilizada para tratar con excepciones. Primero intenta ejecutar el código dentro del bloque try. Si ocurre un error, ejecuta el código dentro del otro bloque. Considere el ejemplo que ve en pantalla.
+*/
+
+try {
+  console.log(num);
+} catch (error) {
+  console.error("Ejemplo"); 
+}
