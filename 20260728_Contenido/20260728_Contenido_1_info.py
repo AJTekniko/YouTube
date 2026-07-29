f9700909fc33a5e04679d7ddb30eff3e9b182240
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-07-28 18:27:00 -0600

    Create 20260728_Contenido_1_info.py

---
+++ 20260728_Contenido/20260728_Contenido_1_info.py
@@ -0,0 +1,12 @@
+"""
+Video: youtube.com/shorts/mOu6UNZeRQU
+
+El método values en Python:
+
+Este método retorna un objeto vista con los valores del diccionario al que se le aplica. Considere el ejemplo que ve en pantalla.
+"""
+
+letras = dict([(1, "a"),
+(2, "b"), (3, "c")])
+
+print(letras.values())
