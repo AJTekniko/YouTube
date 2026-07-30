5c75ffa4cb77a76572f9d0c571fc7291e4e91f79
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-07-30 12:39:58 -0600

    Create 20260730_Contenido_1_info.py

---
+++ 20260730_Contenido/20260730_Contenido_1_info.py
@@ -0,0 +1,12 @@
+"""
+Video: youtube.com/shorts/-PmFY_cvNZc
+
+El método items en Python:
+
+Este método retorna un objeto vista con todos los elementos del diccionario al que se le aplica. Considere el ejemplo que ve en pantalla.
+"""
+
+letras = dict([(1, "a"),
+(2, "b"), (3, "c")])
+
+print(letras.items())
