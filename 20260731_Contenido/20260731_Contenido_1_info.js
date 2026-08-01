1d4f756560f32cabcab503acfea3167116d13c38
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-07-31 23:17:29 -0600

    Create 20260731_Contenido_1_info.js

---
+++ 20260731_Contenido/20260731_Contenido_1_info.js
@@ -0,0 +1,13 @@
+/*
+Video: youtube.com/shorts/7luSU_vsEyc
+
+Las clases en JavaScript:
+
+Las clases son utilizadas para crear objetos y tratar con la herencia. Considere el ejemplo que ve en pantalla.
+*/
+
+class Perro {
+  constructor(nombre) {
+    this.nombre = nombre;
+  }
+}
