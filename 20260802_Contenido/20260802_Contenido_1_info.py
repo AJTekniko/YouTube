b7393a4af9230c812a805c7d1ded9250c296cd9e
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-08-02 19:00:12 -0600

    Create 20260802_Contenido_1_info.py

---
+++ 20260802_Contenido/20260802_Contenido_1_info.py
@@ -0,0 +1,5 @@
+"""
+Video: youtube.com/shorts/nlG1GzF257M
+
+Esta semana, hablé de la declaración try...catch, de la cláusula finally, y de las clases en JavaScript, y de los métodos values() y items() en Python. Muchas gracias por su apoyo.
+"""
