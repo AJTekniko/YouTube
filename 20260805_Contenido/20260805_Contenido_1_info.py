df474c0463f44e09ff4cd02b72272ad8fe6e4206
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-08-05 17:18:27 -0600

    Create 20260805_Contenido_1_info.py

---
+++ 20260805_Contenido/20260805_Contenido_1_info.py
@@ -0,0 +1,11 @@
+"""
+Video: youtube.com/shorts/NGeQYiPijRU
+
+La función set en Python:
+
+Esta función puede ser utilizada para crear conjuntos, incluyendo conjuntos vacíos. Considere el ejemplo que ve en pantalla.
+"""
+
+letras = set("abbccc")
+
+print(letras)
