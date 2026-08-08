16c4bd15b52127878e86ee0a8d0c2ce4250ce88e
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-08-07 18:13:56 -0600

    Create 20260807_Contenido_1_info.py

---
+++ 20260807_Contenido/20260807_Contenido_1_info.py
@@ -0,0 +1,12 @@
+"""
+Video: youtube.com/shorts/OzhYCmyL2C0
+
+El método add en Python:
+
+Este método puede ser utilizado para agregarle un elemento a un conjunto. Considere el ejemplo que ve en pantalla.
+"""
+
+letras = set("abbccc")
+letras.add("d")
+
+print(letras)
