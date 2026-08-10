e6c25aa72d756d5702cd5d11990d744f3efadc36
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-08-10 13:57:26 -0600

    Create 20260810_Contenido_1_info.js

---
+++ 20260810_Contenido/20260810_Contenido_1_info.js
@@ -0,0 +1,12 @@
+/*
+Video: youtube.com/shorts/XgjAdEOpl6A
+
+El método concat en JavaScript:
+
+Este método es utilizado para unir cadenas de caracteres y también puede unir matrices. Considere el ejemplo que ve en pantalla.
+*/
+
+let str1 = "Saludos";
+let str2 = "compañeros";
+
+console.log(str1.concat(" ", str2));
