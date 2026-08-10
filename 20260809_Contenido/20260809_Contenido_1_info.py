5f5a628a1f9f2f98920aa998be0ce6d7aedb0865
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-08-09 18:31:38 -0600

    Create 20260809_Contenido_1_info.py

---
+++ 20260809_Contenido/20260809_Contenido_1_info.py
@@ -0,0 +1,5 @@
+"""
+Video: youtube.com/shorts/Up_-VmOo2GI
+
+Esta semana, hablé del método update(), de la función set(), y del método add() en Python. También cubrí métodos personalizados e instancias de clase en JavaScript. Muchas gracias por su apoyo.
+"""
