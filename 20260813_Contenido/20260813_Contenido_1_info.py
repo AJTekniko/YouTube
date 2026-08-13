2834de4d7c1ad96c363e7c385475b305570a3458
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-08-13 15:46:03 -0600

    Create 20260813_Contenido_1_info.py

---
+++ 20260813_Contenido/20260813_Contenido_1_info.py
@@ -0,0 +1,12 @@
+"""
+Video: youtube.com/shorts/XV54ZZqS-uo
+
+El método issubset en Python:
+
+Este método es utilizado con conjuntos para determinar si los elementos indicados le pertenecen a otro conjunto. Considere el ejemplo que ve en pantalla.
+"""
+
+pares = {2, 4, 6, 8, 10}
+numeros = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+print(pares.issubset(numeros))
