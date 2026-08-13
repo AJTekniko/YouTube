6604ba8c0b894b55e8aaddc4cb0fa9e9c61ebfb9
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-08-12 19:23:15 -0600

    Create 20260812_Contenido_1_info.js

---
+++ 20260812_Contenido/20260812_Contenido_1_info.js
@@ -0,0 +1,11 @@
+/*
+Video: youtube.com/shorts/xy6f_3A2ZE0
+
+La precedencia de operadores en JavaScript:
+
+Este término se refiere al orden en el que se evalúan ciertas partes de una operación: los valores dentro del paréntesis van primero, luego aquellos que son multiplicados o divididos, y después los que son sumados o restados. Si hay operadores con la misma precedencia, se utiliza la asociatividad, el cual indica si los valores deben ser evaluados de izquierda a derecha o de derecha a izquierda. Considere el ejemplo que ve en pantalla.
+*/
+
+console.log(3 + 4 * 5);
+
+console.log(4 * 3 ** 2);
