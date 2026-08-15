e2633b5a4d016fc8ea0b39c1fdb9459e9298733f
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-08-14 20:27:19 -0600

    Create 20260814_Contenido_1_info.js

---
+++ 20260814_Contenido/20260814_Contenido_1_info.js
@@ -0,0 +1,17 @@
+/*
+Video: youtube.com/shorts/xqkZxYAeJLw
+
+Los operadores de incremento y decremento en JavaScript:
+
+Estos operadores son utilizados para aumentar y disminuir el valor del operando por uno. El valor retornado depende en dónde son colocados los operadores, o sea si son usados en forma de prefijo o sufijo. Considere el ejemplo que ve en pantalla.
+*/
+
+let a = 0;
+console.log(++a);
+console.log(a++);
+console.log(a);
+
+let b = 0;
+console.log(--b);
+console.log(b--);
+console.log(b);
