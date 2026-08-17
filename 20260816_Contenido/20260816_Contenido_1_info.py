9981738b089ce538ea15606d980fe98837ab6cd4
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-08-16 19:11:16 -0600

    Create 20260816_Contenido_1_info.py

---
+++ 20260816_Contenido/20260816_Contenido_1_info.py
@@ -0,0 +1,5 @@
+"""
+Video: youtube.com/shorts/4pcEmL0Oca4
+
+Esta semana, hablé del método concat(), de la precedencia de operadores, y de los operadores de incremento y decremento en JavaScript. También cubrí los métodos discard() e issubset() en Python. Muchas gracias por su apoyo.
+"""
