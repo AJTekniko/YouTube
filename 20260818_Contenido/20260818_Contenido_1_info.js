315b489a8d034b90647f718aa068e444e96d377c
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-08-18 16:18:13 -0600

    Create 20260818_Contenido_1_info.js

---
+++ 20260818_Contenido/20260818_Contenido_1_info.js
@@ -0,0 +1,9 @@
+/*
+Video: youtube.com/shorts/m2M2ONswh3o
+
+El operador de estricta igualdad en JavaScript:
+
+Este operador es utilizado para comparar dos operandos. Sin embargo, no realiza coerción de tipos como el operador de igualdad regular. Si los dos operandos tienen el mismo valor, pero son de distinto tipo, los considera diferentes. Considere el ejemplo que ve en pantalla.
+*/
+
+console.log(1 === '1');
