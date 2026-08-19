ccf820df3093b54875152fb5726e42797d430bd5
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-08-19 17:32:13 -0600

    Create 20260819_Contenido_1_info.py

---
+++ 20260819_Contenido/20260819_Contenido_1_info.py
@@ -0,0 +1,13 @@
+"""
+Video: https://youtube.com/shorts/QIvMV1X7DrU
+
+El método isdisjoint en Python:
+
+Este método, que significa "es disjunto", es utilizado para determinar si dos conjuntos no tienen elementos en común. Considere el ejemplo que ve en pantalla.
+"""
+
+pares = {2, 4, 6, 8, 10}
+numeros = {1, 2, 3, 4, 5, 6, 7,
+8, 9, 10}
+
+print(pares.isdisjoint(numeros))
