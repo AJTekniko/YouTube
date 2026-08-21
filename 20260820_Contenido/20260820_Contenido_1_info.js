9445bed7d97ffbed42f24be9c9472e13c5125a24
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-08-20 20:59:23 -0600

    Create 20260820_Contenido_1_info.js

---
+++ 20260820_Contenido/20260820_Contenido_1_info.js
@@ -0,0 +1,9 @@
+/*
+Video: youtube.com/shorts/pYv7kMIQB24
+
+El operador de desigualdad estricta en JavaScript:
+
+Este operador es utilizado para comparar dos operandos. Sin embargo, no realiza coerción de tipos como el operador de desigualdad regular. Si los dos operandos tienen el mismo valor, pero son de distinto tipo, los considera diferentes. Considere el ejemplo que ve en pantalla.
+*/
+
+console.log(1 !== '1');
