5ef4d54e541c35ff8cf3ffbaa1eee75d4562b134
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-08-21 20:39:24 -0600

    Create 20260821_Contenido_1_info.py

---
+++ 20260821_Contenido/20260821_Contenido_1_info.py
@@ -0,0 +1,12 @@
+"""
+Video: youtube.com/shorts/9FRSG0QY5z4
+
+El operador unión en Python:
+
+Este operador retorna un nuevo conjunto con todos los elementos de los conjuntos involucrados. Considere el ejemplo que ve en pantalla.
+"""
+
+pares = {0, 2, 4}
+impares = {1, 3, 5}
+
+print(pares | impares)
