a03d8953f94bf16d313f8c54d54dace9f1addb64
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-08-23 19:08:07 -0600

    Create 20260823_Contenido_1_info.py

---
+++ 20260823_Contenido/20260823_Contenido_1_info.py
@@ -0,0 +1,5 @@
+"""
+Video: youtube.com/shorts/vxUoZZ4xf4M
+
+Esta semana, hablé del método issuperset(), del método isdisjoint(), y del operador unión en Python. También cubrí los operadores de igualdad y desigualdad estricta en JavaScript. Muchas gracias por su apoyo.
+"""
