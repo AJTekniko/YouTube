6acef5eb9f724462b50bf976f38f97cb28ecc816
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-08-25 14:02:48 -0600

    Create 20260825_Contenido_1_info.py

---
+++ 20260825_Contenido/20260825_Contenido_1_info.py
@@ -0,0 +1,12 @@
+"""
+Video: youtube.com/shorts/QiWaW4nnngo
+
+El operador intersección en Python:
+
+Este operador retorna un nuevo conjunto con los elementos que tienen en común los conjuntos involucrados. Considere el ejemplo que ve en pantalla.
+"""
+
+primos = {2, 3, 5}
+impares = {1, 3, 5}
+
+print(primos & impares)
