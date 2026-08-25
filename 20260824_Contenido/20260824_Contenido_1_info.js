ab21326519ab384bfb9c5e274bbcd142db59f604
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-08-24 19:09:17 -0600

    Create 20260824_Contenido_1_info.js

---
+++ 20260824_Contenido/20260824_Contenido_1_info.js
@@ -0,0 +1,13 @@
+/*
+Video: youtube.com/shorts/-FzrEgdc2ZU
+
+El operador unario más en JavaScript:
+
+Este operador puede convertir su operando en un número. Sin embargo, si ya es un número, no hay ningún cambio. Considere el ejemplo que ve en pantalla.
+*/
+
+let str = '5'
+let num = +str;
+
+console.log(num);
+console.log(typeof num);
