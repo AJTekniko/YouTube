58484733f589a39810e16a29f6c501d7b88c4c83
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-08-27 17:25:30 -0600

    Create 20260827_Contenido_1_info.py

---
+++ 20260827_Contenido/20260827_Contenido_1_info.py
@@ -0,0 +1,12 @@
+"""
+Video: www.youtube.com/shorts/gkC1uq0QIaA
+
+El operador diferencia en Python:
+
+Este operador retorna un nuevo conjunto con los elementos que aparecen en el primer conjunto que no están en el segundo. Considere el ejemplo que ve en pantalla.
+"""
+
+primos = {2, 3, 5}
+impares = {1, 3, 5}
+
+print(primos - impares)
