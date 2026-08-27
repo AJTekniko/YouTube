7b6f6b5e766feca87b270c68c1296b9e83f4e1c2
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-08-26 19:21:05 -0600

    Create 20260826_Contenido_1_info.js

---
+++ 20260826_Contenido/20260826_Contenido_1_info.js
@@ -0,0 +1,13 @@
+/*
+Video: youtube.com/shorts/AiNbWUiWyGA
+
+El operador de negación unaria en JavaScript:
+
+Este operador niega su operando y puede convertirlo en un número. Considere el ejemplo que ve en pantalla.
+*/
+
+let str = '5'
+let num = -str;
+
+console.log(num);
+console.log(typeof num);
