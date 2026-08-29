27bb9b2013cf767178cafb066ed87b8798352cde
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-08-28 21:03:26 -0600

    Create 20260828_Contenido_1_info.js

---
+++ 20260828_Contenido/20260828_Contenido_1_info.js
@@ -0,0 +1,12 @@
+/*
+Video: youtube.com/shorts/kiofweJfIGI
+
+El operador ternario en JavaScript:
+
+Este operador se puede utilizar como atajo para sentencias if...else. Si la condición que aparece antes del signo de interrogación es verdad, el operador retorna el valor de la primera expresión, el cual se encuentra después del signo de interrogación. De otro modo, retorna el valor de la expresión que se encuentra después de los dos puntos. Considere el ejemplo que ve en pantalla.
+*/
+
+let edad = 21;
+let gen = edad >= 18 ? "mayor" : "menor";
+
+console.log(gen);
