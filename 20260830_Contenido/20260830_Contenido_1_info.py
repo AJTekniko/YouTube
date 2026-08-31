a9f34ea33333354fc73781b2f9dd9ebd040eb4c1
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-08-30 20:07:19 -0600

    Create 20260830_Contenido_1_info.py

---
+++ 20260830_Contenido/20260830_Contenido_1_info.py
@@ -0,0 +1,5 @@
+"""
+Video: youtube.com/shorts/IgQ0-7wD0NM
+
+Esta semana, hablé del operador unario más, del operador de negación unaria, y del operador ternario en JavaScript. También cubrí los operadores intersección y diferencia en Python. Muchas gracias por su apoyo.
+"""
