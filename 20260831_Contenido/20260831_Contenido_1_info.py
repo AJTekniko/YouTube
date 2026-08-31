4106c5a47423341fa5666adc822c9b6ff1131070
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-08-31 10:03:21 -0600

    Create 20260831_Contenido_1_info.py

---
+++ 20260831_Contenido/20260831_Contenido_1_info.py
@@ -0,0 +1,12 @@
+"""
+Video: youtube.com/shorts/010H47cwa9g
+
+El operador diferencia simétrica en Python:
+
+Este operador retorna un nuevo conjunto con los elementos que aparecen en uno de los dos conjuntos, pero no en los dos. Considere el ejemplo que ve en pantalla.
+"""
+
+primos = {2, 3, 5}
+impares = {1, 3, 5}
+
+print(primos ^ impares)
