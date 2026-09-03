2f5f7c9b98b02a7106ca181a4e39698b3dda71af
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-09-03 13:45:20 -0600

    Create 20260903_Contenido_1_info.js

---
+++ 20260903_Contenido/20260903_Contenido_1_info.js
@@ -0,0 +1,12 @@
+/*
+Video: youtube.com/shorts/RnKA4-Llu4c
+
+Los métodos Math.max y Math.min en JavaScript:
+
+Estos métodos son utilizados para retornar el número más grande y el número más pequeño, respectivamente. Considere los ejemplos que ve en pantalla.
+*/
+
+console.log(Math.max(1, 2, 3));
+
+let ejMatriz = [1, 2, 3];
+console.log(Math.min(...ejMatriz));
