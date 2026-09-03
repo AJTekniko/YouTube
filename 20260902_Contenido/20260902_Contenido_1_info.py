74052d39b99ffdb7c247d6f7af7d64af0ae6a74f
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-09-02 19:16:01 -0600

    Create 20260902_Contenido_1_info.py

---
+++ 20260902_Contenido/20260902_Contenido_1_info.py
@@ -0,0 +1,11 @@
+"""
+Video: youtube.com/shorts/eh_43xHbO6Q
+
+SyntaxError en Python:
+
+Este error ocurre cuando no se sigue la sintaxis de la lengua, como cuando a uno se le olvida cerrar los paréntesis. Considere el ejemplo que ve en pantalla.
+"""
+
+num = 2
+if num % 2 == 0:
+    print("par"
