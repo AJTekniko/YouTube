669b2e28397a0c70e7499fdfacbee8b2a5a9e81a
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-09-04 16:20:12 -0600

    Create 20260904_Contenido_1_info.py

---
+++ 20260904_Contenido/20260904_Contenido_1_info.py
@@ -0,0 +1,9 @@
+"""
+Video: youtube.com/shorts/vSLhqSWpy08
+
+NameError en Python:
+
+Este error, que significa error de nombre, ocurre al intentar de acceder una variable o función que todavía no ha sido definida. Considere el ejemplo que ve en pantalla.
+"""
+
+print(num)
