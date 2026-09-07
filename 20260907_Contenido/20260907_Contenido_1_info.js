c40c91a6c6c29e8f551973e234337bca392ed760
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-09-07 13:31:13 -0600

    Create 20260907_Contenido_1_info.js

---
+++ 20260907_Contenido/20260907_Contenido_1_info.js
@@ -0,0 +1,10 @@
+/*
+Video: youtube.com/shorts/iWqpGjfSbjc
+
+Los métodos Math.ceil y Math.floor en JavaScript:
+
+Estos métodos son utilizados para redeondear el valor al número entero más cercano hacia arriba o hacia abajo, respectivamente. Considere los ejemplos que ve en pantalla.
+*/
+
+console.log(Math.ceil(1.2));
+console.log(Math.floor(3.2));
