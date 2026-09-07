49762518cccf9d39bf3e04249420323aebbdcc5b
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-09-06 18:24:19 -0600

    Create 20260906_Contenido_1_info.py

---
+++ 20260906_Contenido/20260906_Contenido_1_info.py
@@ -0,0 +1,5 @@
+"""
+Video: youtube.com/shorts/4tGl206ZmkE
+
+Esta semana, hablé del operador diferencia simétrica, de SyntaxError, y de NameError en Python. También cubrí el operador de coalescencia nula, y los métodos Math.max y Math.min en JavaScript. Muchas gracias por su apoyo.
+"""
