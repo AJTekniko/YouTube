5f2edeb0bb8030d13285a42f159ea6bc74e49679
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-09-08 13:35:10 -0600

    Create 20260908_Contenido_1_info.py

---
+++ 20260908_Contenido/20260908_Contenido_1_info.py
@@ -0,0 +1,9 @@
+"""
+Video: youtube.com/shorts/lcQJ8Mm0BE4
+
+TypeError en Python:
+
+Este error, que significa error de tipo, ocurre al intentar de realizar una operación con tipos de datos incompatibles. Considere el ejemplo que ve en pantalla.
+"""
+
+print("2" + 3)
