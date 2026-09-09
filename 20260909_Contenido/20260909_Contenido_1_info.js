a8c614b9bbe34cb6fb9d01e2125b39b6ca1b3a5d
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-09-09 15:06:01 -0600

    Create 20260909_Contenido_1_info.js

---
+++ 20260909_Contenido/20260909_Contenido_1_info.js
@@ -0,0 +1,10 @@
+/*
+Video: youtube.com/shorts/pxGAAi7VjzU
+
+El método Math.round en JavaScript:
+
+Este método es utilizado para redondear un valor al número entero más cercano. Considere los ejemplos que ve en pantalla.
+*/
+
+console.log(Math.round(1.2));
+console.log(Math.round(1.7));
