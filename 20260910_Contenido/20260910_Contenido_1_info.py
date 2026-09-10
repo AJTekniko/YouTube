f172318cfe5ee4bf18aec423982dab9421fe953f
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-09-10 13:57:31 -0600

    Create 20260910_Contenido_1_info.py

---
+++ 20260910_Contenido/20260910_Contenido_1_info.py
@@ -0,0 +1,10 @@
+"""
+Video: youtube.com/shorts/oXkJgMhleSc
+
+IndexError en Python:
+
+Este error, que significa error de índice, ocurre al intentar de acceder un índice que no se encuentra en una secuencia. Considere el ejemplo que ve en pantalla.
+"""
+
+saludo = "hola"
+print(saludo[4])
