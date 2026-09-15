9bfb56b08c87540f36a48127813fd92f2b83dc20
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-09-14 19:53:21 -0600

    Create 20260914_Contenido_1_info.js

---
+++ 20260914_Contenido/20260914_Contenido_1_info.js
@@ -0,0 +1,10 @@
+/*
+Video: youtube.com/shorts/Lj1VQwYvriA
+
+El método Math.trunc en JavaScript:
+
+Este método, que significa truncar, es utilizado para eliminar números decimales sin redondear, dejando solo el número entero. Considere los ejemplos que ve en pantalla.
+*/
+
+console.log(Math.trunc(1.2));
+console.log(Math.trunc(1.7));
