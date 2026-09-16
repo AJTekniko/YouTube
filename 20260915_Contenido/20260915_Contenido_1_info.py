8c3aae454a269216206dc3f3cbc7af21dd334b04
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-09-15 19:53:47 -0600

    Create 20260915_Contenido_1_info.py

---
+++ 20260915_Contenido/20260915_Contenido_1_info.py
@@ -0,0 +1,10 @@
+"""
+Video: youtube.com/shorts/mIxkeRLMaKc
+
+AttributeError en Python:
+
+Este error, que significa error de atributo, ocurre al intentar de utilizar un método o una propiedad que no existe en un tipo de objeto. Considere el ejemplo que ve en pantalla.
+"""
+
+saludo = "hola"
+saludo.append(".")
