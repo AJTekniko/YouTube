5a1c3c47cdb12ee29e6fd2946a6029c15fa8aae0
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-09-18 19:19:28 -0600

    Create 20260918_Contenido_1_info.js

---
+++ 20260918_Contenido/20260918_Contenido_1_info.js
@@ -0,0 +1,9 @@
+/*
+Video: youtube.com/shorts/5QI18jU-7dM
+
+El método Math.cbrt en JavaScript:
+
+Este método es utilizado para retornar la raíz cúbica. Considere los ejemplos que ve en pantalla.
+*/
+
+console.log(Math.cbrt(8));
