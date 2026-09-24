60be2e2fed6167655a81e5c6fe6e9357a4fcab30
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-09-23 20:41:18 -0600

    Create 20260923_Contenido_1_info.js

---
+++ 20260923_Contenido/20260923_Contenido_1_info.js
@@ -0,0 +1,9 @@
+/*
+Video: youtube.com/shorts/2NpzVUFU2wc
+
+El método Math.abs en JavaScript:
+
+Este método es utilizado para retornar el valor absoluto de un número. Considere el ejemplo que ve en pantalla.
+*/
+
+console.log(Math.abs(-8));
